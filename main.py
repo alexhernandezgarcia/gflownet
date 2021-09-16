ab2ab6c9f10e4f27a2bd635cb807f347b5e66e92
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2021-09-16 13:07:00 -0400

    black main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)  # annoying numpy err
 def get_config(args, override_args, args2config, save=True):
     """
     Combines YAML configuration file, command line arguments and default arguments into
-    a single configuration dictionary. 
+    a single configuration dictionary.
 
     - Values in YAML file override default values
     - Command line arguments override values in YAML file
@@ -53,6 +53,7 @@ def get_config(args, override_args, args2config, save=True):
     -------
         Namespace
     """
+
     def _update_config(arg, val, config, override=False):
         config_aux = config
         for k in args2config[arg]:
@@ -104,7 +105,7 @@ def add_args(parser):
         type=str,
         help="YAML configuration file",
     )
-    args2config.update({'yaml_config': ['yaml_config']})
+    args2config.update({"yaml_config": ["yaml_config"]})
     # General
     parser.add_argument(
         "--test_mode",
@@ -112,18 +113,18 @@ def add_args(parser):
         default=False,
         help="If true, set parameters for a quick test run",
     )
-    args2config.update({'test_mode': ['test_mode']})
+    args2config.update({"test_mode": ["test_mode"]})
     parser.add_argument("--debug", type=bool, default=False)
-    args2config.update({'debug': ['debug']})
+    args2config.update({"debug": ["debug"]})
     parser.add_argument("--run_num", type=int, default=0, help="Experiment ID")
-    args2config.update({'run_num': ['run_num']})
+    args2config.update({"run_num": ["run_num"]})
     parser.add_argument(
         "--explicit_run_enumeration",
         type=bool,
         default=False,
         help="If True, the next run be fresh, in directory 'run%d'%run_num; if False, regular behaviour. Note: only use this on fresh runs",
     )
-    args2config.update({'explicit_run_enumeration': ['explicit_run_enumeration']})
+    args2config.update({"explicit_run_enumeration": ["explicit_run_enumeration"]})
     # Seeds
     parser.add_argument(
         "--sampler_seed",
@@ -131,128 +132,128 @@ def add_args(parser):
         default=0,
         help="Seed for MCMC modelling (each set of gammas gets a different seed)",
     )
-    args2config.update({'sampler_seed': ['seeds', 'sampler']})
+    args2config.update({"sampler_seed": ["seeds", "sampler"]})
     parser.add_argument(
         "--model_seed",
         type=int,
         default=0,
         help="seed used for model ensemble (each model gets a different seed)",
     )
-    args2config.update({'model_seed': ['seeds','model']})
+    args2config.update({"model_seed": ["seeds", "model"]})
     parser.add_argument(
         "--dataset_seed",
         type=int,
         default=0,
         help="if we are using a toy dataset, it may take a specific seed",
     )
-    args2config.update({'dataset_seed': ['seeds', 'dataset']})
+    args2config.update({"dataset_seed": ["seeds", "dataset"]})
     parser.add_argument(
         "--toy_oracle_seed",
         type=int,
         default=0,
         help="if we are using a toy dataset, it may take a specific seed",
     )
-    args2config.update({'toy_oracle_seed': ['seeds', 'toy_oracle']})
+    args2config.update({"toy_oracle_seed": ["seeds", "toy_oracle"]})
     parser.add_argument(
         "--machine",
         type=str,
         default="local",
         help="'local' or 'cluster' (assumed linux env)",
     )
-    args2config.update({'machine': ['machine']})
+    args2config.update({"machine": ["machine"]})
     parser.add_argument("--device", default="cuda", type=str, help="'cuda' or 'cpu'")
-    args2config.update({'device': ['device']})
+    args2config.update({"device": ["device"]})
     parser.add_argument("--workdir", type=str, default=None, help="Working directory")
-    args2config.update({'workdir': ['workdir']})
+    args2config.update({"workdir": ["workdir"]})
     # Dataset
     parser.add_argument("--dataset", type=str, default="linear")
-    args2config.update({'dataset': ['dataset', 'oracle']})
+    args2config.update({"dataset": ["dataset", "oracle"]})
     parser.add_argument(
         "--dataset_type",
         type=str,
         default="toy",
         help="Toy oracle is very fast to sample",
     )
-    args2config.update({'dataset_type': ['dataset', 'type']})
+    args2config.update({"dataset_type": ["dataset", "type"]})
     parser.add_argument(
         "--init_dataset_length",
         type=int,
         default=int(1e2),
         help="number of items in the initial (toy) dataset",
     )
-    args2config.update({'init_dataset_length': ['dataset', 'init_length']})
+    args2config.update({"init_dataset_length": ["dataset", "init_length"]})
     parser.add_argument(
         "--dict_size",
         type=int,
         default=4,
         help="number of possible choices per-state, e.g., [0,1] would be two, [1,2,3,4] (representing ATGC) would be 4 - with variable length, 0's are added for padding",
     )
-    args2config.update({'dict_size': ['dataset', 'dict_size']})
+    args2config.update({"dict_size": ["dataset", "dict_size"]})
     parser.add_argument(
         "--variable_sample_length",
         type=bool,
         default=True,
         help="models will sample within ranges set below",
     )
-    args2config.update({'variable_sample_length': ['dataset', 'variable_length']})
+    args2config.update({"variable_sample_length": ["dataset", "variable_length"]})
     parser.add_argument("--min_sample_length", type=int, default=10)
-    args2config.update({'min_sample_length': ['dataset' 'min_length']})
+    args2config.update({"min_sample_length": ["dataset" "min_length"]})
     parser.add_argument("--max_sample_length", type=int, default=40)
-    args2config.update({'max_sample_length': ['dataset', 'max_length']})
+    args2config.update({"max_sample_length": ["dataset", "max_length"]})
     parser.add_argument(
         "--sample_tasks",
         type=int,
         default=1,
         help="WIP unfinished for multi-task training - how many outputs per oracle? (only nupack currently  setup for > 1 output)",
     )
-    args2config.update({'sample_tasks': ['dataset', 'sample_tasks']})
+    args2config.update({"sample_tasks": ["dataset", "sample_tasks"]})
     # AL
     parser.add_argument(
         "--sample_method", type=str, default="gflownet", help="'mcmc', 'gflownet'"
     )
-    args2config.update({'sample_method': ['al', 'sample_method']})
+    args2config.update({"sample_method": ["al", "sample_method"]})
     parser.add_argument(
         "--query_mode",
         type=str,
         default="learned",
         help="'random', 'energy', 'uncertainty', 'heuristic', 'learned' # different modes for query construction",
     )
-    args2config.update({'query_mode': ['al', 'query_mode']})
+    args2config.update({"query_mode": ["al", "query_mode"]})
     parser.add_argument(
         "--pipeline_iterations",
         type=int,
         default=1,
         help="number of cycles with the oracle",
     )
-    args2config.update({'pipeline_iterations': ['al', 'n_iter']})
+    args2config.update({"pipeline_iterations": ["al", "n_iter"]})
     parser.add_argument(
         "--query_selection",
         type=str,
         default="clustering",
         help="agglomerative 'clustering', 'cutoff' or strictly 'argmin' based query construction",
     )
-    args2config.update({'query_selection': ['al', 'query_selection']})
+    args2config.update({"query_selection": ["al", "query_selection"]})
     parser.add_argument(
         "--minima_dist_cutoff",
         type=float,
         default=0.25,
         help="minimum distance (normalized, binary) between distinct minima or between clusters in agglomerative clustering OR 'cutoff' batch selection",
     )
-    args2config.update({'minima_dist_cutoff': ['al', 'minima_dist_cutoff']})
+    args2config.update({"minima_dist_cutoff": ["al", "minima_dist_cutoff"]})
     parser.add_argument(
         "--queries_per_iter",
         type=int,
         default=100,
         help="maximum number of questions we can ask the oracle per cycle",
     )
-    args2config.update({'queries_per_iter': ['al', 'queries_per_iter']})
+    args2config.update({"queries_per_iter": ["al", "queries_per_iter"]})
     parser.add_argument(
         "--mode",
         type=str,
         default="training",
         help="'training'  'evaluation' 'initialize' - only training currently useful",
     )
-    args2config.update({'mode': ['al', 'mode']})
+    args2config.update({"mode": ["al", "mode"]})
     # Querier
     parser.add_argument(
         "--model_state_size",
@@ -260,60 +261,62 @@ def add_args(parser):
         default=30,
         help="number of selected datapoints of model evaluations",
     )
-    args2config.update({'model_state_size': ['querier', 'model_state_size']})
+    args2config.update({"model_state_size": ["querier", "model_state_size"]})
     parser.add_argument(
         "--qmodel_opt", type=str, default="SGD", help="optimizer for q-network"
     )
-    args2config.update({'qmodel_opt': ['querier', 'opt']})
+    args2config.update({"qmodel_opt": ["querier", "opt"]})
     parser.add_argument(
         "--qmodel_momentum", type=float, default=0.95, help="momentum for q-network"
     )
-    args2config.update({'qmodel_momentum': ['querier', 'momentum']})
+    args2config.update({"qmodel_momentum": ["querier", "momentum"]})
     parser.add_argument(
         "--qmodel_preload_path",
         type=str,
         default=None,
         help="location of pre-trained qmodel",
     )
-    args2config.update({'qmodel_preload_path': ['querier', 'model_ckpt']})
+    args2config.update({"qmodel_preload_path": ["querier", "model_ckpt"]})
     parser.add_argument("--querier_latent_space_width", type=int, default=10)
-    args2config.update({'querier_latent_space_width': ['querier', 'latent_space_width']})
+    args2config.update(
+        {"querier_latent_space_width": ["querier", "latent_space_width"]}
+    )
     # GFlowNet
     parser.add_argument("--model_ckpt", default=None, type=str)
-    args2config.update({'model_ckpt': ['gflownet' 'model_ckpt']})
+    args2config.update({"model_ckpt": ["gflownet" "model_ckpt"]})
     parser.add_argument("--progress", action="store_true")
-    args2config.update({'progress': ['gflownet', 'progress']})
+    args2config.update({"progress": ["gflownet", "progress"]})
     parser.add_argument(
         "--learning_rate", default=1e-4, help="Learning rate", type=float
     )
-    args2config.update({'learning_rate': ['gflownet', 'learning_rate']})
+    args2config.update({"learning_rate": ["gflownet", "learning_rate"]})
     parser.add_argument("--opt", default="adam", type=str)
-    args2config.update({'opt': ['gflownet', 'opt']})
+    args2config.update({"opt": ["gflownet", "opt"]})
     parser.add_argument("--adam_beta1", default=0.9, type=float)
-    args2config.update({'adam_beta1': ['gflownet', 'adam_beta1']})
+    args2config.update({"adam_beta1": ["gflownet", "adam_beta1"]})
     parser.add_argument("--adam_beta2", default=0.999, type=float)
-    args2config.update({'adam_beta2': ['gflownet', 'adam_beta2']})
+    args2config.update({"adam_beta2": ["gflownet", "adam_beta2"]})
     parser.add_argument("--momentum", default=0.9, type=float)
-    args2config.update({'momentum': ['gflownet', 'momentum']})
+    args2config.update({"momentum": ["gflownet", "momentum"]})
     parser.add_argument("--mbsize", default=16, help="Minibatch size", type=int)
-    args2config.update({'mbsize': ['gflownet', 'mbsize']})
+    args2config.update({"mbsize": ["gflownet", "mbsize"]})
     parser.add_argument("--train_to_sample_ratio", default=1, type=float)
-    args2config.update({'train_to_sample_ratio': ['gflownet', 'train_to_sample_ratio']})
+    args2config.update({"train_to_sample_ratio": ["gflownet", "train_to_sample_ratio"]})
     parser.add_argument("--n_hid", default=256, type=int)
-    args2config.update({'n_hid': ['gflownet', 'n_hid']})
+    args2config.update({"n_hid": ["gflownet", "n_hid"]})
     parser.add_argument("--n_layers", default=2, type=int)
-    args2config.update({'n_layers': ['gflownet', 'n_layers']})
+    args2config.update({"n_layers": ["gflownet", "n_layers"]})
     parser.add_argument(
         "--n_train_steps", default=20000, type=int, help="gflownet training steps"
     )
-    args2config.update({'n_train_steps': ['gflownet', 'n_train_steps']})
+    args2config.update({"n_train_steps": ["gflownet", "n_train_steps"]})
     parser.add_argument(
         "--num_empirical_loss",
         default=200000,
         type=int,
         help="Number of samples used to compute the empirical distribution loss",
     )
-    args2config.update({'num_empirical_loss': ['gflownet', 'num_empirical_loss']})
+    args2config.update({"num_empirical_loss": ["gflownet", "num_empirical_loss"]})
     parser.add_argument(
         "--batch_reward",
         type=bool,
@@ -326,18 +329,18 @@ def add_args(parser):
         default=1000,
         help="Sequences to sample from GFLowNet",
     )
-    args2config.update({'gflownet_n_samples': ['gflownet', 'n_samples']})
-    args2config.update({'batch_reward': ['gflownet', 'batch_reward']})
+    args2config.update({"gflownet_n_samples": ["gflownet", "n_samples"]})
+    args2config.update({"batch_reward": ["gflownet", "batch_reward"]})
     parser.add_argument("--bootstrap_tau", default=0.0, type=float)
-    args2config.update({'bootstrap_tau': ['gflownet', 'bootstrap_tau']})
+    args2config.update({"bootstrap_tau": ["gflownet", "bootstrap_tau"]})
     parser.add_argument("--clip_grad_norm", default=0.0, type=float)
-    args2config.update({'clip_grad_norm': ['gflownet', 'clip_grad_norm']})
+    args2config.update({"clip_grad_norm": ["gflownet", "clip_grad_norm"]})
     parser.add_argument("--comet_project", default=None, type=str)
-    args2config.update({'comet_project': ['gflownet', 'comet', 'project']})
+    args2config.update({"comet_project": ["gflownet", "comet", "project"]})
     parser.add_argument(
         "-t", "--tags", nargs="*", help="Comet.ml tags", default=[], type=str
     )
-    args2config.update({'tags': ['gflownet', 'comet', 'tags']})
+    args2config.update({"tags": ["gflownet", "comet", "tags"]})
     # Proxy model
     parser.add_argument(
         "--proxy_model_type",
@@ -345,53 +348,53 @@ def add_args(parser):
         default="mlp",
         help="type of proxy model - mlp or transformer",
     )
-    args2config.update({'proxy_model_type': ['proxy', 'model_type']})
+    args2config.update({"proxy_model_type": ["proxy", "model_type"]})
     parser.add_argument(
         "--training_parallelism",
         type=bool,
         default=False,
         help="fast enough on GPU without paralellism - True doesn't always work on linux",
     )
-    args2config.update({'training_parallelism': ['proxy', 'training_parallelism']})
+    args2config.update({"training_parallelism": ["proxy", "training_parallelism"]})
     parser.add_argument(
         "--proxy_model_ensemble_size",
         type=int,
         default=10,
         help="number of models in the ensemble",
     )
-    args2config.update({'proxy_model_ensemble_size': ['proxy', 'ensemble_size']})
+    args2config.update({"proxy_model_ensemble_size": ["proxy", "ensemble_size"]})
     parser.add_argument(
         "--proxy_model_width",
         type=int,
         default=256,
         help="number of neurons per proxy NN layer",
     )
-    args2config.update({'proxy_model_width': ['proxy', 'width']})
+    args2config.update({"proxy_model_width": ["proxy", "width"]})
     parser.add_argument(
         "--embedding_dim",
         type=int,
         default=256,
         help="embedding dimension for transformer only",
     )
-    args2config.update({'embedding_dim': ['proxy', 'embedding_dim']})
+    args2config.update({"embedding_dim": ["proxy", "embedding_dim"]})
     parser.add_argument(
         "--proxy_model_layers",
         type=int,
         default=2,
         help="number of layers in NN proxy models (transformer encoder layers OR MLP layers)",
     )
-    args2config.update({'proxy_model_layers': ['proxy', 'n_layers']})
+    args2config.update({"proxy_model_layers": ["proxy", "n_layers"]})
     parser.add_argument("--proxy_training_batch_size", type=int, default=10)
-    args2config.update({'proxy_training_batch_size': ['proxy', 'mbsize']})
+    args2config.update({"proxy_training_batch_size": ["proxy", "mbsize"]})
     parser.add_argument("--proxy_max_epochs", type=int, default=200)
-    args2config.update({'proxy_max_epochs': ['proxy', 'max_epochs']})
+    args2config.update({"proxy_max_epochs": ["proxy", "max_epochs"]})
     parser.add_argument(
         "--proxy_shuffle_dataset",
         type=bool,
         default=True,
         help="give each model in the ensemble a uniquely shuffled dataset",
     )
-    args2config.update({'proxy_shuffle_dataset': ['proxy', 'shuffle_dataset']})
+    args2config.update({"proxy_shuffle_dataset": ["proxy", "shuffle_dataset"]})
     # MCMC
     parser.add_argument(
         "--mcmc_sampling_time",
@@ -399,19 +402,20 @@ def add_args(parser):
         default=int(1e4),
         help="at least 1e4 is recommended for convergence",
     )
-    args2config.update({'mcmc_sampling_time': ['mcmc', 'sampling_time']})
+    args2config.update({"mcmc_sampling_time": ["mcmc", "sampling_time"]})
     parser.add_argument(
         "--mcmc_num_samplers",
         type=int,
         default=40,
         help="minimum number of gammas over which to search for each sampler (if doing in parallel, we may do more if we have more CPUs than this)",
     )
-    args2config.update({'mcmc_num_samplers': ['mcmc', 'num_samplers']})
+    args2config.update({"mcmc_num_samplers": ["mcmc", "num_samplers"]})
     parser.add_argument("--stun_min_gamma", type=float, default=-3)
-    args2config.update({'stun_min_gamma': ['mcmc', 'stun_min_gamma']})
+    args2config.update({"stun_min_gamma": ["mcmc", "stun_min_gamma"]})
     parser.add_argument("--stun_max_gamma", type=float, default=1)
-    args2config.update({'stun_max_gamma': ['mcmc', 'stun_max_gamma']})
+    args2config.update({"stun_max_gamma": ["mcmc", "stun_max_gamma"]})
     return parser, args2config
+
 
 def process_config(config):
     # Normalize seeds
@@ -463,7 +467,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     config = get_config(args, override_args, args2config)
     config = process_config(config)
-    import ipdb; ipdb.set_trace()
+    import ipdb
+
+    ipdb.set_trace()
     print("Args:\n" + "\n".join([f"    {k:20}: {v}" for k, v in vars(config).items()]))
     al = activeLearner.ActiveLearning(params)
     if params.mode == "initalize":
