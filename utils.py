05fc81171505e1afbf87b3de6d66ea51eb067eed
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2021-09-16 13:05:13 -0400

    dict2namespace

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 '''import statement'''
+from argparse import Namespace
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -533,3 +534,28 @@ class resultsPlotter():
         plt.clim(1,self.niters)
         plt.colorbar()
         plt.tight_layout()
+
+def dict2namespace(data_dict):
+    """
+    Recursively converts a dictionary and its internal dictionaries into an
+    argparse.Namespace
+
+    Parameters
+    ----------
+    data_dict : dict
+        The input dictionary
+
+    Return
+    ------
+    data_namespace : argparse.Namespace
+        The output namespace
+    """
+    for k, v in data_dict.items():
+        if isinstance(v, dict):
+            data_dict[k] = dict2namespace(v)
+        else:
+            pass
+    data_namespace = Namespace(**data_dict)
+
+    return data_namespace
+
