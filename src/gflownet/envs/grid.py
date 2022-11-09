862de1526284147baf603f661c69056ac4c6e4c7
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2022-11-09 09:53:53 -0500

    update imports to adapt to package

--- src/gflownet/envs/grid.py
+++ src/gflownet/envs/grid.py
@@ -4,7 +4,7 @@ Classes to represent a hyper-grid environments
 import itertools
 import numpy as np
 import pandas as pd
-from gflownetenv import GFlowNetEnv
+from src.gflownet.envs.base import GFlowNetEnv
 
 
 class Grid(GFlowNetEnv):
