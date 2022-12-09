d223df44a5a908ec8cc776e83c48c958b4356b94
nikita-0209 <nikitasaxena0209@gmail.com>
2022-12-09 13:37:18 -0500

    update readme and clean file

--- playground/botorch/mes_nn_like_gp.py
+++ playground/botorch/mes_nn_like_gp.py
@@ -11,27 +11,27 @@ from torch.nn import Sequential, ReLU, Dropout
 from torch import tensor
 import numpy as np
 from abc import ABC
+from botorch.acquisition.max_value_entropy_search import qMaxValueEntropy
+from botorch.models.model import Model
+from gpytorch.distributions import MultivariateNormal, MultitaskMultivariateNormal
+from botorch.posteriors.gpytorch import GPyTorchPosterior
+# from botorch.posteriors.
+from torch import distributions
 
 
 """
-Tutorial: https://botorch.org/tutorials/compare_mc_analytic_acquisition
+Initialise the Dataset 
 """
-
 neg_hartmann6 = Hartmann(dim=6, negate=True)
 
 train_x = torch.rand(10, 6)
 train_y = neg_hartmann6(train_x).unsqueeze(-1)
 
+
 """
-If we are doing DropoutRegressor technique, then it must have a dropout layer
+Initialise and train the NN
 """
 mlp = Sequential(Linear(6, 1024), ReLU(), Dropout(0.5), Linear(1024, 1024), Dropout(0.5), ReLU(), Linear(1024, 1))
-
-"""
-For a very sparse network, with hidden_dim = 8, the expected improvement is very less even though the final loss of the mlp is similar.
-EI dec as MLP dim dec
-"""
-
 NUM_EPOCHS = 10
 
 mlp.train()
@@ -52,14 +52,9 @@ for epoch in range(NUM_EPOCHS):
          )
     optimizer.step()
 
-
-
-from botorch.acquisition.max_value_entropy_search import qMaxValueEntropy
-from botorch.models.model import Model
-from gpytorch.distributions import MultivariateNormal, MultitaskMultivariateNormal
-from botorch.posteriors.gpytorch import GPyTorchPosterior
-# from botorch.posteriors.
-from torch import distributions
+"""
+Derived class of botorch.Models that implements posterior
+"""
 
 class NN_Model(Model):
     def __init__(self, nn):
@@ -67,52 +62,23 @@ class NN_Model(Model):
         self.model = nn
         self._num_outputs = 1
         self.nb_samples = 20
-        """
-        train_inputs: A `n_train x d` Tensor that the model has been fitted on.
-                Not required if the model is an instance of a GPyTorch ExactGP model.
-        """
-        # self.train_inputs = train_x
-
-    # def _shaped_noise_covar(self, base_shape: torch.Size):
-    #     return self.noise_covar(*params, shape=base_shape, **kwargs)
 
     def posterior(self, X, observation_noise = False, posterior_transform = None):
         super().posterior(X, observation_noise, posterior_transform)
         self.model.train()
-        # if observation_noise is not False:
-
-        # if len(X.shape) == 4:
-        #     X = X.squeeze(-2)
 
         with torch.no_grad():
              outputs = torch.hstack([self.model(X) for _ in range(self.nb_samples)])
         mean = torch.mean(outputs, axis=1)
         var = torch.var(outputs, axis=1)
 
-        # if len(X.shape) == 4:
-            # mean = mean.squeeze(-2)
-            # var = var.squeeze(-2)
-        # mean = mean.unsqueeze(-1)
-        # var = var.unsqueeze(-1)
-        """
-        Remark: issue arises because posterior is called twice.
-        first for the sampler:
-            now when sampler calls, then mean = 10, var = 10
-        but when forward class
-            then mean = 10*1 and var= 10x1
-        """
         if len(X.shape)==2:
             covar = torch.diag(var)
         elif len(X.shape)==4:
-            var_element = var[0]
             covar = [torch.diag(var[i][0]) for i in range(X.shape[0])]
             covar = torch.stack(covar, axis = 0)
             covar = covar.unsqueeze(-1)
-        # if len(X.shape)!=4:
-            # mvn = MultitaskMultivariateNormal(mean, covar)
-        # else:
         mvn = MultivariateNormal(mean, covar)
-        tmvn = distributions.MultivariateNormal(mean, covar)
         posterior = GPyTorchPosterior(mvn)
         return posterior
 
@@ -133,40 +99,23 @@ class NN_Model(Model):
 proxy = NN_Model(mlp)
 
 qMES = qMaxValueEntropy(proxy, candidate_set = train_x, num_fantasies=1, use_gumbel=False)
-test_X=torch.tensor([[[0.8754, 0.9025, 0.5862, 0.1580, 0.3266, 0.7930]],
-
-        [[0.1407, 0.2835, 0.0574, 0.7165, 0.2836, 0.8033]],
-
-        [[0.1043, 0.4672, 0.7695, 0.5995, 0.2715, 0.7897]],
-
-        [[0.6130, 0.8399, 0.3882, 0.2005, 0.5959, 0.5445]],
-
-        [[0.5849, 0.9051, 0.8367, 0.1182, 0.3853, 0.9588]],
-
-        [[0.4114, 0.7935, 0.0299, 0.3348, 0.1985, 0.3097]],
-
-        [[0.0172, 0.8890, 0.6926, 0.1963, 0.3057, 0.2855]],
-
-        [[0.6131, 0.9267, 0.6613, 0.1429, 0.3706, 0.3486]],
-
-        [[0.5914, 0.8657, 0.4393, 0.6715, 0.7866, 0.7446]],
-
-        [[0.6269, 0.9950, 0.0640, 0.4415, 0.1140, 0.6024]]])
  
-#because in the forward call, qMES adds a dimension but it also 
-# does not accept textX in shape b x 1 xd
+
+"""
+Create 10k batches of test data points to check whether the MES gives negative values for any of those configurations.
+"""
 for num in range(10000):
     test_x = torch.rand(10, 6)
     test_x = test_x.unsqueeze(-2)
     with torch.no_grad():
-        mes = qMES(test_X)
+        mes = qMES(test_x)
         mes_arr = mes.detach().numpy()
         verdict = np.all(mes_arr>0)
     if not verdict:
         print(mes)
 
 """
-Randomly predicted values with negative elements: 
+Randomly predicted MES vals with negative elements: 
 tensor([ 1.3287e-04,  2.7284e-02, -5.9893e-03,  4.2737e-05,  1.0618e-01,
          4.2081e-05,  1.2386e-04,  3.4879e-02,  5.4181e-05,  7.9588e-03])
 tensor([ 8.3982e-03, -1.9492e-02,  9.3261e-02,  4.2140e-05,  1.1625e-01,
