bb60a7faaba9954438914f1e29162d132e41cf7d
nikita-0209 <nikitasaxena0209@gmail.com>
2022-12-13 16:20:19 -0500

    quasi wokring config

--- playground/botorch/qmes_nn.py
+++ playground/botorch/qmes_nn.py
@@ -1,6 +1,4 @@
 import torch
-
-# from botorch.fit import fit_gpytorch_mll
 from botorch.models import SingleTaskGP
 from botorch.test_functions import Hartmann
 from gpytorch.mlls import ExactMarginalLogLikelihood
@@ -11,27 +9,30 @@ from torch.nn import Sequential, ReLU, Dropout
 from torch import tensor
 import numpy as np
 from abc import ABC
-
-from botorch.acquisition.max_value_entropy_search import qMaxValueEntropy, qLowerBoundMaxValueEntropy
+from botorch.acquisition.max_value_entropy_search import qMaxValueEntropy
 from botorch.models.model import Model
 from gpytorch.distributions import MultivariateNormal, MultitaskMultivariateNormal
 from botorch.posteriors.gpytorch import GPyTorchPosterior
 from torch import distributions
+from botorch.models.utils import check_no_nans
+from gpytorch.utils.cholesky import psd_safe_cholesky
+
+CLAMP_LB = 1.0e-8
 
 """
-Create the Dataset
+Initialise the Dataset 
 """
-
 neg_hartmann6 = Hartmann(dim=6, negate=True)
+
 train_x = torch.rand(10, 6)
 train_y = neg_hartmann6(train_x).unsqueeze(-1)
 
+
 """
-Initialise and Train a MLP
+Initialise and train the NN
 """
 mlp = Sequential(Linear(6, 1024), ReLU(), Dropout(0.5), Linear(1024, 1024), Dropout(0.5), ReLU(), Linear(1024, 1))
-
-NUM_EPOCHS = 10
+NUM_EPOCHS = 0
 
 mlp.train()
 optimizer = Adam(mlp.parameters())
@@ -51,11 +52,9 @@ for epoch in range(NUM_EPOCHS):
          )
     optimizer.step()
 
-
 """
-Derived CLass ftrom botorch.models.Model ton implement the posrterio function
+Derived class of botorch.Models that implements posterior
 """
-
 
 class NN_Model(Model):
     def __init__(self, nn):
@@ -74,15 +73,17 @@ class NN_Model(Model):
         var = torch.var(outputs, axis=1)
 
         if len(X.shape)==2:
-            covar = np.cov(outputs, ddof =1, rowvar=True)
-            covar = torch.FloatTensor(covar)
+            # covar = torch.cov(outputs)
+            covar = torch.diag(var)
         elif len(X.shape)==4:
             covar = [torch.diag(var[i][0]) for i in range(X.shape[0])]
             covar = torch.stack(covar, axis = 0)
             covar = covar.unsqueeze(-1)
-
+        elif len(X.shape)==3:
+            covar = [torch.diag(var[i]) for i in range(X.shape[0])]
+            covar = torch.stack(covar, axis = 0)
+        
         mvn = MultivariateNormal(mean, covar)
-        tmvn = distributions.MultivariateNormal(mean, covar)
         posterior = GPyTorchPosterior(mvn)
         return posterior
 
@@ -102,8 +103,9 @@ class NN_Model(Model):
 
 proxy = NN_Model(mlp)
 
-# qMES = qMaxValueEntropy(proxy, candidate_set = train_x, num_fantasies=1, use_gumbel=False)
-qMES = qLowerBoundMaxValueEntropy(proxy, candidate_set = train_x, use_gumbel=False)
+from botorch.acquisition.max_value_entropy_search import qLowerBoundMaxValueEntropy
+
+qMES = qLowerBoundMaxValueEntropy(proxy, candidate_set = train_x, use_gumbel=True)
 
 for num in range(10000):
     test_x = torch.rand(10, 6)
@@ -113,20 +115,4 @@ for num in range(10000):
         mes_arr = mes.detach().numpy()
         verdict = np.all(mes_arr>0)
     if not verdict:
-        print(mes)
-
-"""
-Randomly predicted values with negative elements: 
-tensor([ 1.3287e-04,  2.7284e-02, -5.9893e-03,  4.2737e-05,  1.0618e-01,
-         4.2081e-05,  1.2386e-04,  3.4879e-02,  5.4181e-05,  7.9588e-03])
-tensor([ 8.3982e-03, -1.9492e-02,  9.3261e-02,  4.2140e-05,  1.1625e-01,
-         4.2021e-05,  6.3142e-03,  8.2910e-04,  4.2707e-05,  1.0034e-04])
-tensor([ 6.0743e-02, -6.0380e-02,  1.1793e-01,  4.3750e-05,  1.6773e-01,
-         7.5281e-05,  2.9400e-02,  6.6042e-05,  4.2379e-05,  4.2036e-05])
-tensor([ 1.4783e-03, -5.4467e-04,  1.3299e-01,  4.9710e-05,  2.4702e-02,
-         4.1842e-05,  8.1807e-05,  1.2018e-02,  4.2468e-05,  2.6655e-02])
-tensor([ 1.1917e-02, -2.6156e-02,  2.2992e-01,  4.4107e-05,  1.6128e-01,
-         4.2021e-05,  4.4733e-05,  4.6474e-04,  4.2200e-05,  4.2727e-02])
-tensor([ 3.2218e-02,  4.7796e-03,  2.1847e-01,  4.4882e-05, -1.0960e-01,
-         4.1842e-05,  3.1891e-04,  4.2439e-05,  1.7229e-04,  1.8108e-04])
-"""
+        print("Negative MES", mes)
