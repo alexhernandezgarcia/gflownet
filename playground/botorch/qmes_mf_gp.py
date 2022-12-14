11689b6bafe010199d5605b458a0d921a4ddafb5
nikita-0209 <nikitasaxena0209@gmail.com>
2022-12-13 23:53:27 -0500

    working mf mes with gibbon

---
+++ playground/botorch/qmes_mf_gp.py
@@ -0,0 +1,162 @@
+import torch
+
+# from botorch.fit import fit_gpytorch_mll
+from botorch.models import SingleTaskGP
+from botorch.test_functions import Hartmann, AugmentedHartmann
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from torch.optim import Adam
+from torch.nn import Linear
+from torch.nn import MSELoss
+from torch.nn import Sequential, ReLU, Dropout
+from torch import tensor
+import numpy as np
+from abc import ABC
+import gpytorch
+from gpytorch.priors.torch_priors import GammaPrior
+from botorch.models.utils import check_no_nans
+from gpytorch.utils.cholesky import psd_safe_cholesky
+from botorch.models.model import Model
+
+CLAMP_LB = 1.0e-8
+
+# neg_hartmann6 = Hartmann(dim=6, negate=True)
+neg_hartmann6 = AugmentedHartmann(negate=True)
+fidelities = torch.tensor([0.5, 0.75, 1.0])
+
+train_seq = torch.rand(10, 6)
+train_f = fidelities[torch.randint(3, (10,1))]
+train_x = torch.cat((train_seq, train_f), dim=1)
+train_y = neg_hartmann6(train_x).unsqueeze(-1)
+
+# We will use the simplest form of GP model, exact inference
+class ExactGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
+        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean(batch_shape=torch.Size([]))
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.MaternKernel(
+                    nu=2.5,
+                    ard_num_dims=train_x.shape[-1],
+                    batch_shape=torch.Size([]),
+                    lengthscale_prior=GammaPrior(3.0, 6.0),
+                ),
+                batch_shape=torch.Size([]),
+                outputscale_prior=GammaPrior(2.0, 0.15),
+            )
+
+    def forward(self, x):
+        mean_x = self.mean_module(x) #101
+        covar_x = self.covar_module(x) #(train+test, train+test)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+# initialize likelihood and model
+likelihood = gpytorch.likelihoods.GaussianLikelihood()
+gp = ExactGPModel(train_x, train_y, likelihood)
+
+gp.train()
+likelihood.train()
+mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, gp)
+
+optimizer = Adam([{'params': gp.parameters()}], lr=0.1)
+
+
+NUM_EPOCHS = 10
+
+for epoch in range(NUM_EPOCHS):
+    optimizer.zero_grad()
+    output = gp(train_x)
+    loss = - mll(output, gp.train_targets).mean()
+    loss.backward()
+    if (epoch + 1) % 10 == 0:
+        print(
+            f"Epoch {epoch+1:>3}/{NUM_EPOCHS} - Loss: {loss.item():>4.3f} " 
+         )
+    optimizer.step()
+
+from gpytorch.distributions import MultivariateNormal, MultitaskMultivariateNormal
+from botorch.posteriors.gpytorch import GPyTorchPosterior
+from botorch.models.utils import add_output_dim
+from gpytorch.likelihoods.gaussian_likelihood import FixedNoiseGaussianLikelihood
+from gpytorch.models.exact_prediction_strategies import prediction_strategy
+from gpytorch.utils.broadcasting import _mul_broadcast_shape
+
+
+
+class myGPModel(SingleTaskGP):
+    def __init__(self, gp, trainX=None, trainY=None):
+        super().__init__(trainX, trainY)
+        self.model = gp
+    
+    @property
+    def num_outputs(self) -> int:
+        return super().num_outputs
+
+    @property
+    def batch_shape(self):
+        """
+        This is a batch shape from an I/O perspective. For a model with `m` outputs, a `test_batch_shape x q x d`-shaped input `X`
+        to the `posterior` method returns a Posterior object over an output of
+        shape `broadcast(test_batch_shape, model.batch_shape) x q x m`.
+
+        """
+        return super().batch_shape
+
+    def posterior(self, X, output_indices = None, observation_noise= False, posterior_transform= None):
+        """
+        Args:
+            X: A `(batch_shape) x q x d`-dim Tensor, where `d` is the dimension
+                of the feature space and `q` is the number of points considered
+                jointly.
+            output_indices: A list of indices, corresponding to the outputs over
+                which to compute the posterior (if the model is multi-output).
+                Can be used to speed up computation if only a subset of the
+                model's outputs are required for optimization. If omitted,
+                computes the posterior over all model outputs.
+            observation_noise: If True, add the observation noise from the
+                likelihood to the posterior. If a Tensor, use it directly as the
+                observation noise (must be of shape `(batch_shape) x q x m`).
+            posterior_transform: An optional PosteriorTransform.
+        Returns:
+            A `GPyTorchPosterior` object, representing `batch_shape` joint
+            distributions over `q` points and the outputs selected by
+            `output_indices` each. Includes observation noise if specified.
+        """
+        self.eval()  # make sure model is in eval mode
+        # X = self.transform_inputs(X)
+        if self._num_outputs > 1:
+            X, output_dim_idx = add_output_dim(
+                X=X, original_batch_shape=self._input_batch_shape
+            )
+        
+        mvn = self(X) #self.forward() does not work here.
+        # I mean it does not give any error, but it gives the same mean and
+        # covariance matrix for all the test points. I don't know why.
+        posterior = GPyTorchPosterior(mvn=mvn)
+        if np.all(posterior.mean.detach().numpy()<0):
+            print(posterior.mean.numpy())
+        if np.all(posterior.mvn.covariance_matrix.detach().numpy()<0):
+            print(posterior.mvn.covariance_matrix.numpy())
+        return posterior
+
+from botorch.acquisition.max_value_entropy_search import qLowerBoundMaxValueEntropy, qMultiFidelityLowerBoundMaxValueEntropy
+proxy = myGPModel(gp, train_x, train_y)
+
+from botorch.acquisition.utils import project_to_target_fidelity
+target_fidelities = {6: 1.0}
+def project(X):
+    return project_to_target_fidelity(X=X, target_fidelities=target_fidelities)
+
+qMES = qMultiFidelityLowerBoundMaxValueEntropy(proxy, candidate_set = train_x, project = project)
+
+for num in range(10000):
+    test_seq = torch.rand(10, 6)
+    test_f = fidelities[torch.randint(3, (10,1))]
+    test_x = torch.cat((test_seq, test_f), dim=1)
+    test_x = test_x.unsqueeze(-2)
+    with torch.no_grad():
+        mes = qMES(test_x)
+        mes_arr = mes.detach().numpy()
+        verdict = np.all(mes_arr>0)
+    if not verdict:
+        print("Negative MES", mes)
+        # print(mes)
---
+++ playground/botorch/qmes_mf_nn.py
@@ -0,0 +1,144 @@
+import torch
+from botorch.models import SingleTaskGP
+from botorch.test_functions import AugmentedHartmann
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from torch.optim import Adam
+from torch.nn import Linear
+from torch.nn import MSELoss, Module
+from torch.nn import Sequential, ReLU, Dropout
+from torch import tensor
+import numpy as np
+from abc import ABC
+from botorch.acquisition.max_value_entropy_search import qMaxValueEntropy
+from botorch.models.model import Model
+from gpytorch.distributions import MultivariateNormal, MultitaskMultivariateNormal
+from botorch.posteriors.gpytorch import GPyTorchPosterior
+from torch import distributions
+from botorch.models.utils import check_no_nans
+from gpytorch.utils.cholesky import psd_safe_cholesky
+
+CLAMP_LB = 1.0e-8
+
+"""
+Initialise the Dataset 
+"""
+neg_hartmann6 = AugmentedHartmann(negate=True)
+fidelities = torch.tensor([0.5, 0.75, 1.0])
+
+
+train_seq = torch.rand(10, 6)
+train_f = fidelities[torch.randint(3, (10,1))]
+train_x = torch.cat((train_seq, train_f), dim=1)
+train_y = neg_hartmann6(train_x).unsqueeze(-1)
+
+
+"""
+Initialise and train the NN
+"""
+class MLP(Module):
+    def __init__(self):
+        super().__init__()
+        self.model = Sequential(Linear(6, 1024), ReLU(), Dropout(0.5), Linear(1024, 1024), Dropout(0.5), ReLU(), Linear(1024, 1))
+    def forward(self, x):
+        x = self.preprocess(x)
+        output = self.model(x)
+        return output
+    def preprocess(self, x):
+        # write code to tkae the first elment along the last axis of x
+        # x = x
+        x = x[..., :-1]
+        return x
+
+mlp = MLP()
+mlp.train()
+optimizer = Adam(mlp.parameters())
+criterion = MSELoss()
+
+seed = 123
+torch.manual_seed(seed)
+NUM_EPOCHS = 1
+
+for epoch in range(NUM_EPOCHS):
+    optimizer.zero_grad()
+    output = mlp(train_x)
+    loss = criterion(output, train_y)
+    loss.backward()
+    if (epoch + 1) % 10 == 0:
+        print(
+            f"Epoch {epoch+1:>3}/{NUM_EPOCHS} - Loss: {loss.item():>4.3f} "
+         )
+    optimizer.step()
+
+"""
+Derived class of botorch.Models that implements posterior
+"""
+
+class NN_Model(Model):
+    def __init__(self, nn):
+        super().__init__()
+        self.model = nn
+        self._num_outputs = 1
+        self.nb_samples = 20
+
+    def posterior(self, X, observation_noise = False, posterior_transform = None):
+        super().posterior(X, observation_noise, posterior_transform)
+        self.model.train()
+
+        with torch.no_grad():
+             outputs = torch.hstack([self.model(X) for _ in range(self.nb_samples)])
+        mean = torch.mean(outputs, axis=1)
+        var = torch.var(outputs, axis=1)
+
+        if len(X.shape)==2:
+            covar = torch.diag(var)
+        elif len(X.shape)==4:
+            mean = mean.squeeze(-1).unsqueeze(1)
+            outputs = outputs.squeeze(-1)
+            outputs = outputs.view(X.shape[0], -1, self.nb_samples)
+            covar = [torch.cov(outputs[i]) for i in range(X.shape[0])]
+            covar = torch.stack(covar, axis = 0)
+            covar = covar.unsqueeze(1)
+        elif len(X.shape)==3:
+            covar = [torch.diag(var[i]) for i in range(X.shape[0])]
+            covar = torch.stack(covar, axis = 0)
+        
+        mvn = MultivariateNormal(mean, covar)
+        posterior = GPyTorchPosterior(mvn)
+        return posterior
+
+    @property
+    def num_outputs(self) -> int:
+        return self._num_outputs
+
+    @property
+    def batch_shape(self):
+        """
+        This is a batch shape from an I/O perspective. For a model with `m` outputs, a `test_batch_shape x q x d`-shaped input `X`
+        to the `posterior` method returns a Posterior object over an output of
+        shape `broadcast(test_batch_shape, model.batch_shape) x q x m`.
+
+        """
+        return torch.Size([])
+
+proxy = NN_Model(mlp)
+
+from botorch.acquisition.utils import project_to_target_fidelity
+target_fidelities = {6: 1.0}
+def project(X):
+    return project_to_target_fidelity(X=X, target_fidelities=target_fidelities)
+
+
+from botorch.acquisition.max_value_entropy_search import qMultiFidelityLowerBoundMaxValueEntropy
+qMES = qMultiFidelityLowerBoundMaxValueEntropy(proxy, candidate_set = train_x, project = project)
+
+for num in range(10000):
+    test_seq = torch.rand(10, 6)
+    test_f = fidelities[torch.randint(3, (10,1))]
+    test_x = torch.cat((test_seq, test_f), dim=1)
+    test_x = test_x.unsqueeze(-2)
+    with torch.no_grad():
+        mes = qMES(test_x)
+        mes_arr = mes.detach().numpy()
+        verdict = np.all(mes_arr>0)
+    if not verdict:
+        print("Negative MES", mes)
