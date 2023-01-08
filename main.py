3e4cba63950f9eb559710a2da6aa4a0f8d4d350d
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-01-07 21:25:36 -0500

    write samples to csv at the end of training

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ Runnable script with hydra capabilities
 """
 from comet_ml import Experiment
 import sys
+import os
 import random
 import hydra
+import pandas as pd
 from omegaconf import OmegaConf, DictConfig
 from gflownet.utils.common import flatten_config
 
@@ -21,13 +23,23 @@ def main(config):
     proxy = hydra.utils.instantiate(config.proxy)
     # The proxy is passed to env and used for computing rewards
     env = hydra.utils.instantiate(config.env, proxy=proxy)
-    gflownet = hydra.utils.instantiate(config.gflownet, env=env, buffer=config.env.buffer)
+    gflownet = hydra.utils.instantiate(
+        config.gflownet, env=env, buffer=config.env.buffer
+    )
     gflownet.train()
 
-    # sample from the oracle, not from a proxy model
-    batch, times = gflownet.sample_batch(
-        env, config.n_samples, train=False
-    )
+    # Sample from trained GFlowNet
+    if config.n_samples > 0 and config.n_samples <= 1e5:
+        samples, times = gflownet.sample_batch(env, config.n_samples, train=False)
+        energies = env.oracle(env.state2oracle(samples))
+        df = pd.DataFrame(
+            {
+                "readable": [env.state2readable(s) for s in samples],
+                "samples": [s for s in samples],
+                "energies": energies,
+            }
+        )
+        df.to_csv("gfn_samples.csv")
     print(gflownet.buffer.replay)
 
 
