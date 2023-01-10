3a7ae0816e6017a72e14d3dc68660e7844cb7cd3
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-01-10 15:21:54 -0500

    generalisation of corners proxy to continuous case; should work with discrete too

--- gflownet/proxy/corners.py
+++ gflownet/proxy/corners.py
@@ -1,15 +1,36 @@
+from typing import List
 from gflownet.proxy.base import Proxy
 import numpy as np
 
 
-class GridCorners(Proxy):
-    def __init__(self):
+class Corners(Proxy):
+    """
+    It is assumed that the state values will be in the range [-1.0, 1.0].
+    """
+    def __init__(self, n_dim=None, mu=None, sigma=None):
         super().__init__()
+        self.n_dim = n_dim
+        self.mu = mu
+        self.sigma = sigma
+        self.mulnormal = self.setup()
 
-    def __call__(self, inputs):
+    def setup(self):
+        if self.sigma and self.mu and self.n_dim:
+            self.mu_vec = np.repeat(self.mu, self.n_dim)
+            self.cov = self.sigma * np.eye(self.n_dim)
+            self.cov_det = np.linalg.det(self.cov)
+            self.cov_inv = np.linalg.inv(self.cov)
+            self.mulnormal_norm = 1.0 / ((2 * np.pi) ** 2 * self.cov_det) ** 0.5
+            self.mulnormal = True
+        else:
+            self.mulnormal = False
+        return self.mulnormal
+
+    def __call__(self, state_list: List[List]):
         """
         args:
-            inputs: list of arrays in desired format interpretable by oracle
+            state_list: list of states
+
         returns:
             list of scores
         technically an oracle, hence used variable name energies
@@ -24,4 +45,20 @@ class GridCorners(Proxy):
             )
             return energies
 
-        return np.asarray([_func_corners(x) for x in inputs])
+        def _mulnormal_corners(x):
+            return -1.0 * self.mulnormal_norm * np.exp(
+                -0.5
+                * (
+                    np.diag(
+                        np.dot(
+                            np.dot((np.abs(x) - self.mu_vec), self.cov_inv),
+                            (np.abs(x) - self.mu_vec).T,
+                        )
+                    )
+                )
+            )
+
+        if self.mulnormal:
+            return _mulnormal_corners(np.array(state_list))
+        else:
+            return np.asarray([_func_corners(state) for state in state_list])
