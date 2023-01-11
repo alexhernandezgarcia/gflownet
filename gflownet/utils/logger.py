e631165db581ed9527ee7afcbf146b4f1fd92aaf
nikita-0209 <nikitasaxena0209@gmail.com>
2023-01-11 12:44:42 -0500

    add train, test, oracle loggging functions

--- gflownet/utils/logger.py
+++ gflownet/utils/logger.py
@@ -4,14 +4,14 @@ from datetime import datetime
 import numpy as np
 
 
-class wandb_logger:
+class Logger:
     """
     Utils functions to compute and handle the statistics (saving them or send to
     wandb). It can be passed on to querier, gfn, proxy, ... to get the
     statistics of training of the generated data at real time
     """
 
-    def __init__(self, config, project_name, run_name=None):
+    def __init__(self, config, project_name, sampler, run_name=None, tags=None):
         self.config = config
         if run_name is None:
             date_time = datetime.today().strftime("%d/%m-%H:%M:%S")
@@ -20,6 +20,7 @@ class wandb_logger:
             )
         self.run = wandb.init(config=config, project=project_name, name=run_name)
         self.add_tags(config.log.tags)
+        self.sampler = sampler
         self.context = "0"
 
     def add_tags(self, tags):
@@ -51,6 +52,64 @@ class wandb_logger:
             for key, _ in metrics.items():
                 key = self.context + "/" + key
         wandb.log(metrics, step)
+
+    def log_sampler_train(self, rewards, proxy_vals, states_term, data, it):
+        if it % self.sampler.train == 0:
+            self.logger.log_metrics(
+                dict(
+                    (
+                        [
+                            "mean_reward{}".format(self.al_iter),
+                            "max_reward{}".format(self.al_iter),
+                            "mean_proxy{}".format(self.al_iter),
+                            "min_proxy{}".format(self.al_iter),
+                            "max_proxy{}".format(self.al_iter),
+                            "mean_seq_length{}".format(self.al_iter),
+                            "batch_size{}".format(self.al_iter),
+                        ],
+                        [
+                            np.mean(rewards),
+                            np.max(rewards),
+                            np.mean(proxy_vals),
+                            np.min(proxy_vals),
+                            np.max(proxy_vals),
+                            np.mean([len(state) for state in states_term]),
+                            len(data),
+                        ],
+                    )
+                ),
+                self.use_context,
+                step=it,
+            )
+
+    def log_sampler_test(self, corr, data_logq, it):
+        if it % self.sampler.test == 0:
+            self.logger.log_metrics(
+                dict(
+                    zip(
+                        [
+                            "test_corr_logq_score",
+                            "test_mean_log",
+                        ],
+                        [
+                            corr[0, 1],
+                            np.mean(data_logq),
+                        ],
+                    )
+                ),
+                self.use_context,
+                step=it,
+            )
+
+    def log_sampler_oracle(self, energies, it):
+        if it % self.sampler.oracle == 0:
+            dict_topk = {}
+            for k in self.oracle_k:
+                mean_topk = np.mean(energies[:k])
+                dict_topk.update(
+                    {"oracle_mean_top{}{}".format(k, self.al_iter): mean_topk}
+                )
+            self.logger.log_metrics(dict_topk, self.use_context)
 
     def end(self):
         wandb.finish()
