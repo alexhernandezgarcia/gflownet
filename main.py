490a717ffc26ec1cc4e367ef9dae32baeeb50362
nikita-0209 <nikitasaxena0209@gmail.com>
2023-01-14 20:11:07 -0500

    checkout main.py from logger and update with evaluate() call

--- main.py
+++ main.py
@@ -2,38 +2,69 @@
 Runnable script with hydra capabilities
 """
 import sys
+import os
 import random
 import hydra
+import pandas as pd
+import yaml
 from omegaconf import OmegaConf, DictConfig
 from gflownet.utils.common import flatten_config
-import numpy as np
-import copy
 
 
-@hydra.main(config_path="./config", config_name="main")
+@hydra.main(config_path="./config", config_name="main", version_base="1.1")
 def main(config):
+    # Get current directory and set it as root log dir for Logger
+    cwd = os.getcwd()
+    config.logger.logdir.root = cwd
     # Reset seed for job-name generation in multirun jobs
     random.seed(None)
+    # Set other random seeds
+    set_seeds(config.seed)
     # Log config
+    # TODO: Move log config to Logger
     log_config = flatten_config(OmegaConf.to_container(config, resolve=True), sep="/")
     log_config = {"/".join(("config", key)): val for key, val in log_config.items()}
-    logger = None
-    if config.log.skip == False:
-        logger = hydra.utils.instantiate(config.logger, config, _recursive_=False)
+    with open(cwd + "/config.yml", "w") as f:
+        yaml.dump(log_config, f, default_flow_style=False)
+
+    # Logger
+    logger = hydra.utils.instantiate(config.logger, config, _recursive_=False)
     # The proxy is required in the env for scoring: might be an oracle or a model
-    proxy = hydra.utils.instantiate(config.proxy, device=config.device)
+    proxy = hydra.utils.instantiate(config.proxy)
     # The proxy is passed to env and used for computing rewards
     env = hydra.utils.instantiate(config.env, proxy=proxy)
     gflownet = hydra.utils.instantiate(
-        config.gflownet, env=env, buffer=config.env.buffer, device=config.device, logger=logger,
-        log_dir=config.logger.logdir,
+        config.gflownet,
+        env=env,
+        buffer=config.env.buffer,
+        logger=logger,
     )
     gflownet.train()
 
-    batch, times = gflownet.sample_batch(env, config.n_samples, train=False)
-    _, _, _ = gflownet.evaluate(
-        batch, oracle=proxy, performance=True, diversity=True, novelty=False
-    )
+    # Sample from trained GFlowNet
+    if config.n_samples > 0 and config.n_samples <= 1e5:
+        samples, times = gflownet.sample_batch(env, config.n_samples, train=False)
+        energies = env.oracle(env.statebatch2oracle(samples))
+        _, _, _ = gflownet.evaluate(
+        samples, oracle=proxy, performance=True, diversity=True, novelty=False
+        )
+        df = pd.DataFrame(
+            {
+                "readable": [env.state2readable(s) for s in samples],
+                "energies": energies,
+            }
+        )
+        df.to_csv("gfn_samples.csv")
+    print(gflownet.buffer.replay)
+
+def set_seeds(seed):
+    import torch
+    import numpy as np
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
 
 if __name__ == "__main__":
