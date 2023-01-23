58b39a6835b6e1de426bbdc0f21772c92f2c1447
AlexandraVolokhova <s-volohova@yandex.ru>
2023-01-23 13:00:55 -0500

    add new mol proxe

--- gflownet/envs/ad_molecule_simple.py
+++ gflownet/envs/ad_molecule_simple.py
@@ -78,31 +78,54 @@ class ADMoleculeSimple(ContinuousTorus):
 
     def statetorch2proxy(
         self, states: TensorType["batch", "state_dim"]
-    ) -> List[Tuple[npt.NDArray, npt.NDArray]]:
+    ) -> npt.NDArray:
         """
-        Prepares a batch of states in torch "GFlowNet format" for the proxy.
+        Prepares a batch of states in torch "GFlowNet format" for the oracle.
         """
         device = states.device
         if device == torch.device("cpu"):
             np_states = states.numpy()
         else:
             np_states = states.cpu().numpy()
-        result = self.statebatch2proxy(np_states)
-        return result
+        return np_states[:, :-1]
 
     def statebatch2proxy(
         self, states: List[List]
-    ) -> List[Tuple[npt.NDArray, npt.NDArray]]:
+    ) -> npt.NDArray:
         """
         Prepares a batch of states in "GFlowNet format" for the proxy: a tensor where
         each state is a row of length n_dim with an angle in radians. The n_actions
         item is removed.
         """
-        states_proxy = []
+        return np.array(states)[:, :-1]
+    
+    def statetorch2oracle(
+        self, states: TensorType["batch", "state_dim"]
+    ) -> List[Tuple[npt.NDArray, npt.NDArray]]:
+        """
+        Prepares a batch of states in torch "GFlowNet format" for the oracle.
+        """
+        device = states.device
+        if device == torch.device("cpu"):
+            np_states = states.numpy()
+        else:
+            np_states = states.cpu().numpy()
+        result = self.statebatch2oracle(np_states)
+        return result
+
+    def statebatch2oracle(
+        self, states: List[List]
+    ) -> List[Tuple[npt.NDArray, npt.NDArray]]:
+        """
+        Prepares a batch of states in "GFlowNet format" for the oracle: a list of tuples, 
+        where first element in the tuple is numpy array of atom positions of shape [num_atoms, 3]
+        and the second element is numpy array of atomic numbers of shape [num_atoms, ]
+        """
+        states_oracle = []
         for st in states:
             conf = self.sync_conformer_with_state(st)
-            states_proxy.append((conf.get_atom_positions(), conf.get_atomic_numbers()))
-        return states_proxy
+            states_oracle.append((conf.get_atom_positions(), conf.get_atomic_numbers()))
+        return states_oracle
 
     # def reset(self):
     #     super().reset()
--- gflownet/proxy/molecule.py
+++ gflownet/proxy/molecule.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.typing as npt
+import pickle
 import torch
 
 from xtb.interface import Calculator, Param, XTBException
@@ -7,54 +8,26 @@ from xtb.libxtb import VERBOSITY_MUTED
 
 
 from gflownet.proxy.base import Proxy
+from sklearn.ensemble import RandomForestRegressor
 
 
-class MoleculeEnergy(Proxy):
-    def __init__(self):
+class MoleculeEnergyProxy(Proxy):
+    def __init__(self, path_to_model):
         super().__init__()
-
-    def __call__(self, states_proxy):
-        # todo: probably make it parallel with mpi
-        return torch.tensor(
-            [self.get_energy(*st) for st in states_proxy],
-            dtype=self.float,
-            device=self.device,
-        )
-
+        with open(path_to_model, 'rb') as inp:
+            self.model = pickle.load(inp)
+    
     def set_device(self, device):
         self.device = device
 
     def set_float_precision(self, dtype):
         self.float = dtype
 
-    def get_energy(
-        self,
-        atom_positions: npt.NDArray[np.float32],
-        atomic_numbers: npt.NDArray[np.int64],
-    ) -> float:
-        """
-        Compute energy of a molecule defined by atom_positions and atomic_numbers
-        """
-        calc = Calculator(Param.GFN2xTB, atomic_numbers, atom_positions)
-        calc.set_verbosity(VERBOSITY_MUTED)
-        try:
-            res = calc.singlepoint()
-        except XTBException as exc:
-            print(exc)
-            print("try again")
-            try:
-                res = calc.singlepoint()
-            except XTBException as exc:
-                print(exc)
-                print("try again")
-                res = calc.singlepoint()
-        return res.get_energy()
-
-
-if __name__ == "__main__":
-    from gflownet.utils.molecule.conformer_base import get_dummy_ad_conf_base
-
-    conf = get_dummy_ad_conf_base()
-    proxy = MoleculeEnergy()
-    energy = proxy.get_energy(conf.get_atom_positions(), conf.get_atomic_numbers())
-    print("energy", energy)
+    def __call__(self, states_proxy):
+        # output of the model is exp(-energy) / 100
+        rewards = -np.log(self.model.predict(states_proxy) * 100)
+        return torch.tensor(
+            rewards,
+            dtype=self.float,
+            device=self.device,
+        )
