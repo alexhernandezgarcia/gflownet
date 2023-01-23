4474dab37b5688c456c68b05e6e34248636d50d4
nikita-0209 <nikitasaxena0209@gmail.com>
2023-01-23 18:07:25 -0500

    device as input arg to proxy

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def main(config):
     # Logger
     logger = hydra.utils.instantiate(config.logger, config, _recursive_=False)
     # The proxy is required in the env for scoring: might be an oracle or a model
+    # 
     proxy = hydra.utils.instantiate(config.proxy, device=config.device)
     # The proxy is passed to env and used for computing rewards
     env = hydra.utils.instantiate(config.env, proxy=proxy)
