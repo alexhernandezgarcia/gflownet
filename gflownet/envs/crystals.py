64e9d17a522422f0f6d09a0dfb29bec1e9484a48
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-01-23 09:55:47 -0500

    remove old code

--- gflownet/envs/crystals.py
+++ gflownet/envs/crystals.py
@@ -5,7 +5,7 @@ from typing import List
 import itertools
 import numpy as np
 import pandas as pd
-from src.gflownet.envs.base import GFlowNetEnv
+from gflownet.envs.base import GFlowNetEnv
 
 
 class Crystal(GFlowNetEnv):
@@ -325,48 +325,4 @@ class Crystal(GFlowNetEnv):
                 valid = True
                 self.n_actions += 1
             return self.state, self.eos, valid
-
-    @staticmethod
-    def func_corners(x_list):
-        def _func_corners(x):
-            ax = abs(x)
-            return -1.0 * (
-                (ax > 0.5).prod(-1) * 0.5
-                + ((ax < 0.8) * (ax > 0.6)).prod(-1) * 2
-                + 1e-1
-            )
-
-        return np.asarray([_func_corners(x) for x in x_list])
-
-    @staticmethod
-    def func_corners_floor_B(x_list):
-        def _func_corners_floor_B(x_list):
-            ax = abs(x)
-            return -1.0 * (
-                (ax > 0.5).prod(-1) * 0.5
-                + ((ax < 0.8) * (ax > 0.6)).prod(-1) * 2
-                + 1e-2
-            )
-
-        return np.asarray([_func_corners_floor_B(x) for x in x_list])
-
-    @staticmethod
-    def func_corners_floor_A(x_list):
-        def _func_corners_floor_A(x_list):
-            ax = abs(x)
-            return -1.0 * (
-                (ax > 0.5).prod(-1) * 0.5
-                + ((ax < 0.8) * (ax > 0.6)).prod(-1) * 2
-                + 1e-3
-            )
-
-        return np.asarray([_func_corners_floor_A(x) for x in x_list])
-
-    @staticmethod
-    def func_cos_N(x_list):
-        def _func_cos_N(x_list):
-            ax = abs(x)
-            return -1.0 * (((np.cos(x * 50) + 1) * norm.pdf(x * 5)).prod(-1) + 0.01)
-
-        return np.asarray([_func_cos_N(x) for x in x_list])
 
