3097aae3e4808f78d4025b71e2f97f75cd361c34
nikita-0209 <nikitasaxena0209@gmail.com>
2023-01-27 12:30:26 -0500

    correct step and get_parents()

--- gflownet/envs/mfenv.py
+++ gflownet/envs/mfenv.py
@@ -5,77 +5,88 @@ from torchtyping import TensorType
 from .base import GFlowNetEnv
 import numpy as np
 
+
 class MultiFidelityEnvWrapper(GFlowNetEnv):
     """
     Multi-fidelity environment for GFlowNet.
+    Assumes same data transformation required for all oracles.
+    Does not require the different oracles as scoring is performed by GFN not env
     """
-    def __init__(self, env, n_fid, oracle):
+
+    def __init__(self, env, n_fid):
         self.env = env
         self.n_fid = n_fid
-        self.oracle = oracle
         self.fid = self.env.eos + 1
+        # Member variables of env are also member variables of MFENV
+        vars(self).update(vars(self.env))
         self.action_space = self.get_actions_space()
         self.fixed_policy_output = self.get_fixed_policy_output()
-        # Assumes that all oracle srequired the same kind of transformed dtata
-        self.state2oracle = self.env.state2oracle
+        self.policy_input_dim = len(self.state2policy())
+        self.random_policy_output = self.fixed_policy_output
+        # Assumes that all oracles required the same kind of transformed dtata
+        # self.state2oracle = self.env.state2oracle
+        # self.oracle = self.env.oracle
         self.reset()
-        # Fid in the state should range from 0 to self.n_fid -1 for easy one hot encoding
 
-    def get_actions_space(self): 
-        actions =  self.env.get_actions_space()
+    def get_actions_space(self):
+        actions = self.env.get_actions_space()
         for fid_index in range(self.n_fid):
-            actions = actions +[(self.fid, fid_index)] 
+            actions = actions + [(self.fid, fid_index)]
+        return actions
 
-    def get_fixed_policy_output(self):
-        output = self.env.get_fixed_policy_output()
-        fixed_policy_output = np.concatenate(output, np.ones((self.n_fid)))
-        return fixed_policy_output
-
-    def reset(self, env_id=None, fid: int = None):
+    def reset(self, env_id=None, fid: int = -1):
+        # Fid in the state should range from 0 to self.n_fid -1 for easy one hot encoding
         # Offline Trajectory: might want to reset the env with a specific fid
-        # TODO: env is a member variable of MFENV
-        # TODO: all member variables of env are also members of MFENV. Is this desired?
+        # TODO: does member variable env need to be updated?
+        # env is a member variable of MFENV because we need the functions
         self.env = self.env.reset(env_id)
-        # TODO: check if fid as a member variable is required.
-        self.fid = fid
-        self.state = self.env.state + [self.fid]
+        # If fid is required as a member variable
+        # then it should not be named self.fid as self.fid represents fidelity action
+        self.state = self.env.state + [fid]
+        # Update the variables that were reset and are required in mf
+        self.done = self.env.done
+        self.id = self.env.id
         return self
 
     def get_mask_invalid_actions_forward(self, state=None, done=None):
         if state is None:
-            state = self.state
+            state = self.state.copy()
+        if done is None:
             done = self.done
+        if done:
+            return [True for _ in range(len(self.action_space))]
         mask = self.env.get_mask_invalid_actions_forward(state[:-1], done)
-        # if fidelity has not been chosen, done is invalid and fid is valid
         if state[-1] == -1:
-            mask[-1] = True
-            mask = + [False]
-        # if fidelity has been chosen then fid is invalid
+            mask = mask + [False for _ in range(self.n_fid)]
         else:
-            mask=+[True]
+            mask = mask + [True for _ in range(self.n_fid)]
         return mask
-        # TODO: do we need check done if fid is invalid?
 
     def state2policy(self, state: List = None):
-        state_proxy = self.env.state2policy(state[:-1])
-        fid_proxy = np.zeros((self.n_fid), dtype=np.float32)
-        fid_proxy[state[-1]] = 1
-        return np.concatenate((state_proxy, fid_proxy), axis=0)
+        if state is None:
+            state = self.state.copy()
+        state_policy = self.env.state2policy(state[:-1])
+        fid_policy = np.zeros((self.n_fid), dtype=np.float32)
+        if state[-1] != -1:
+            fid_policy[state[-1]] = 1
+        state_fid_policy = np.concatenate((state_policy, fid_policy), axis=0)
+        return state_fid_policy
 
     def statebatch2policy(self, states: List[List]) -> npt.NDArray[np.float32]:
-        # Remark: Fidelity cannot be -1 for any state here
-        # Feed states (without fidelity) to state2policy
-        # print last element of each state in states
-        # print(states[:, -1])
-        state_policy =  self.env.statebatch2policy(states[:, -1])
+        state_policy, fid_list = zip(*[(state[:-1], state[-1]) for state in states])
+        state_policy = self.env.statebatch2policy(state_policy)
         fid_policy = np.zeros((len(states), self.n_fid), dtype=np.float32)
-        fid_policy[np.arange(len(states)), states[:, -1]] = 1
-        return np.concatenate((state_policy, fid_policy), axis=1)
+        fid_array = np.array(fid_list)
+        index = np.where(fid_array != -1)[0]
+        if index.size:
+            fid_policy[index, fid_array[index]] = 1
+        state_fid_policy = np.concatenate((state_policy, fid_policy), axis=1)
+        return state_fid_policy
 
     def policy2state(self, state_policy: List) -> List:
-        policy_no_fid = state_policy[:, :-self.n_fid]
+        policy_no_fid = state_policy[:, : -self.n_fid]
         state_no_fid = self.env.policy2state(policy_no_fid)
-        fid = np.argmax(state_policy[:, -self.n_fid:], axis=1)
+        fid = np.argmax(state_policy[:, -self.n_fid :], axis=1)
         state = state_no_fid + fid.to_list()
         return state
 
@@ -91,28 +102,36 @@ class MultiFidelityEnvWrapper(GFlowNetEnv):
         return state
 
     def get_parents(self, state=None, done=None, action=None):
-        parents, actions = super().get_parents(state[:, :-1], done, action)
-        # TODO: optimize code
-        for idx, s in enumerate(state):
-            if s[-1]==-1:
-                fid = s[-1]
-                parent = s[:-1] + [-1]
-                actions[idx].append(self.action_space[fid+self.fid])
-                parents[idx].append(parent)
-        return parents, actions
-
-    def step(self, action, state=None, done = None):
+        assert self.state[:-1] == self.env.state
         if state is None:
-            state = self.state
+            state = self.state.copy()
         if done is None:
             done = self.done
-        if done:
-            return state, action, False
-        if action[0] == self.fid:
-            state[-1] = action[1]
-            return state, action, True
-        else:
+        parents_no_fid, actions = self.env.get_parents(state[:-1])
+        parents = [parent + [-1] for parent in parents_no_fid]
+        # TODO: if fidelity has been chosen,
+        # parents = parents + [parent + [fid] for parent in parents]
+        if state[-1] != -1:
             fid = state[-1]
-            state, action, done = self.env.step(action, state[:-1], done)
-            state = state + [fid]
-            return state, action, done
+            parent = state[:-1] + [-1]
+            actions.append((self.fid, fid))
+            parents.append(parent)
+        return parents, actions
+
+    def step(self, action):
+        assert self.state[:-1] == self.env.state
+        if self.done:
+            return self.state, action, False
+        if action[0] == self.fid:
+            # unnecessary to check because already implemented in  mask
+            if self.state[-1] != -1:
+                return state, action, False
+            self.state[-1] = action[1]
+            if self.env.done == True:
+                self.done = True
+            return self.state, action, True
+        else:
+            fid = self.state[-1]
+            state, action, valid = self.env.step(action)
+            self.state = state + [fid]
+            return self.state, action, valid
