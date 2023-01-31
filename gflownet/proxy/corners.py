4f5e4046a207008e4c3ac2c300648776e07b642c
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-01-31 12:52:32 -0500

    wip: maker corners proxy torch

--- gflownet/proxy/corners.py
+++ gflownet/proxy/corners.py
@@ -1,6 +1,7 @@
 from gflownet.proxy.base import Proxy
 import numpy as np
-import numpy.typing as npt
+import torch
+from torchtyping import TensorType
 
 
 class Corners(Proxy):
@@ -8,8 +9,8 @@ class Corners(Proxy):
     It is assumed that the state values will be in the range [-1.0, 1.0].
     """
 
-    def __init__(self, n_dim=None, mu=None, sigma=None):
-        super().__init__()
+    def __init__(self, n_dim=None, mu=None, sigma=None, **kwargs):
+        super().__init__(**kwargs)
         self.n_dim = n_dim
         self.mu = mu
         self.sigma = sigma
@@ -17,17 +18,18 @@ class Corners(Proxy):
 
     def setup(self):
         if self.sigma and self.mu and self.n_dim:
-            self.mu_vec = np.repeat(self.mu, self.n_dim)
-            self.cov = self.sigma * np.eye(self.n_dim)
-            self.cov_det = np.linalg.det(self.cov)
-            self.cov_inv = np.linalg.inv(self.cov)
-            self.mulnormal_norm = 1.0 / ((2 * np.pi) ** 2 * self.cov_det) ** 0.5
-            self.mulnormal = True
+            self.mu_vec = self.mu * torch.ones(self.n_dim, device=self.device, dtype=self.float)
+            cov = self.sigma * torch.eye(self.n_dim, device=self.device, dtype=self.float)
+            cov_det = torch.linalg.det(cov)
+            self.cov_inv = torch.linalg.inv(cov)
+            self.mulnormal_norm = 1.0 / ((2 * torch.pi) ** 2 * cov_det) ** 0.5
+#             self.mulnormal = True
+            self.mulnormal = False
         else:
             self.mulnormal = False
         return self.mulnormal
 
-    def __call__(self, states: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+    def __call__(self, states: TensorType["batch", "state_dim"]) -> TensorType["batch"]:
         """
         args:
             states: ndarray
@@ -47,16 +49,17 @@ class Corners(Proxy):
             return energies
 
         def _mulnormal_corners(x):
+            import ipdb; ipdb.set_trace()
             return (
                 -1.0
                 * self.mulnormal_norm
-                * np.exp(
+                * torch.exp(
                     -0.5
                     * (
-                        np.diag(
-                            np.dot(
-                                np.dot((np.abs(x) - self.mu_vec), self.cov_inv),
-                                (np.abs(x) - self.mu_vec).T,
+                        torch.diag(
+                            torch.dot(
+                                torch.dot((torch.abs(x) - self.mu_vec), self.cov_inv),
+                                (torch.abs(x) - self.mu_vec).T,
                             )
                         )
                     )
@@ -64,6 +67,10 @@ class Corners(Proxy):
             )
 
         if self.mulnormal:
+            import ipdb; ipdb.set_trace()
             return _mulnormal_corners(states)
         else:
+            snp = states.cpu().numpy()
+            pnp =  np.asarray([_func_corners(state) for state in snp])
+            import ipdb; ipdb.set_trace()
             return np.asarray([_func_corners(state) for state in states])
