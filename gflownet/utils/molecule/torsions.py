b9be53c7dc58b9fb777a33c16480badc2576af9f
AlexandraVolokhova <s-volohova@yandex.ru>
2023-02-28 18:55:38 -0500

    implemented apply rotations

--- gflownet/utils/molecule/torsions.py
+++ gflownet/utils/molecule/torsions.py
@@ -2,6 +2,10 @@ import torch
 import networkx as nx
 import numpy as np
 
+from pytorch3d.transforms import axis_angle_to_matrix
+
+from gflownet.utils.molecule import constants
+
 def get_rotation_masks(dgl_graph):
     """
     :param dgl_graph: the dgl.Graph object with bidirected edges in the order: [e_1_fwd, e_1_bkw, e_2_fwd, e_2_bkw, ...] 
@@ -25,3 +29,32 @@ def get_rotation_masks(dgl_graph):
     edges_mask = torch.from_numpy(bonds_mask.repeat(2))
     nodes_mask = torch.from_numpy(nodes_mask.repeat(2, axis=0))
     return edges_mask, nodes_mask
+
+def apply_rotations(dgl_graph, rotations):
+    """
+    Apply rotations (torsion angles updates)
+    :param dgl_graph: bidirectional dgl.Graph
+    :param rotations: a sequence of torsion angle updates of length = number of bonds in the molecule.
+    The order corresponds to the order of edges in the graph, such that action[i] is
+    an update for the torsion angle corresponding to the edge[2i]
+    """
+    pos = graph.ndata[constants.atom_position_name]
+    edge_mask = graph.edata[constants.rotatable_edges_mask_name]
+    node_mask = graph.edata[constants.rotation_affected_nodes_mask_name]
+    edges = torch.stack(graph.edges()).T
+    # TODO check how slow it is and whether it's possible to vectorise this loop
+    for idx_update, update in enumerate(rotations):
+        idx_edge = idx_update * 2
+        if edge_mask[idx_edge]:
+            begin_pos = pos[edges[idx_edge][0]]
+            end_pos = pos[edges[idx_edge][1]]
+            rot_vector = end_pos - begin_pos
+            rot_vector = rot_vector / torch.linalg.norm(rot_vector) * update
+            rot_matrix = axis_angle_to_matrix(rot_vector)
+            x = pos[node_mask[idx_edge]]
+            pos[node_mask[idx_edge]] = torch.matmul((x - begin_pos), rot_matrix.T) + begin_pos
+    dgl_graph.ndata[constants.atom_position_name] = pos
+    return dgl_graph
+
+
+
