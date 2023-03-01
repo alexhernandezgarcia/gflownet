cf7216c15c22606ec85b5893f67aac7c7e2c104d
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-03-01 12:43:53 -0500

    basic gflownet test

--- tests/gflownet/envs/common.py
+++ tests/gflownet/envs/common.py
@@ -1,4 +1,7 @@
+import hydra
 import numpy as np
+from hydra import compose, initialize
+
 
 def test__get_parents_step_get_mask__are_compatible(env, n=100):
     for traj in range(n):
@@ -13,7 +16,7 @@ def test__get_parents_step_get_mask__are_compatible(env, n=100):
             for p, p_a in zip(parents, parents_a):
                 mask = env.get_mask_invalid_actions_forward(p, False)
                 assert p_a in env.action_space
-                assert mask[env.action_space.index(p_a)] == False
+                assert mask[env.action_space.index(p_a)] is False
 
 
 def test__get_parents__returns_no_parents_in_initial_state(env):
@@ -21,3 +24,32 @@ def test__get_parents__returns_no_parents_in_initial_state(env):
     assert len(parents) == 0
     assert len(actions) == 0
 
+
+def test__gflownet_minimal_runs(env):
+    # Load config
+    with initialize(version_base="1.1", config_path="../../../config", job_name="xxx"):
+        config = compose(config_name="tests")
+    # Logger
+    logger = hydra.utils.instantiate(config.logger, config, _recursive_=False)
+    # Proxy
+    proxy = hydra.utils.instantiate(
+        config.proxy, device=config.device, float_precision=config.float_precision
+    )
+    # Set proxy in env
+    env.proxy = proxy
+    # No buffer
+    config.env.buffer.train = None
+    config.env.buffer.test = None
+    # Set 1 training step
+    config.gflownet.optimizer.n_train_steps = 1
+    # GFlowNet agent
+    gflownet = hydra.utils.instantiate(
+        config.gflownet,
+        device=config.device,
+        float_precision=config.float_precision,
+        env=env,
+        buffer=config.env.buffer,
+        logger=logger,
+    )
+    gflownet.train()
+    assert True
--- tests/gflownet/envs/test_grid.py
+++ tests/gflownet/envs/test_grid.py
@@ -1,14 +1,17 @@
-import pytest
-import torch
-import numpy as np
-from gflownet.envs.grid import Grid
 import common
+import pytest
+
+from gflownet.envs.grid import Grid
+
 
 @pytest.fixture
 def env():
-        return Grid(n_dim=3, length=5)
+    return Grid(n_dim=2, length=5)
+
 
 def test__get_parents_step_get_mask__are_compatible(env):
     return common.test__get_parents_step_get_mask__are_compatible(env)
 
 
+def test__gflownet_minimal_runs(env):
+    return common.test__gflownet_minimal_runs(env)
