e08e578fada9d12d24ab74f795fe8aa44959a48a
vict0rsch <vsch@pm.me>
2023-03-02 12:00:25 -0500

    configure sphinx

---
+++ docs/conf.py
@@ -0,0 +1,69 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "GFlowNet"
+copyright = "2023, Alex Hernandez-Garcia"
+author = "Alex Hernandez-Garcia"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx_math_dollar",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "collapse_navigation": False,
+    "navigation_depth": 6,
+}
+
+# -- Extension configuration -------------------------------------------------
+
+# https://sphinx-autoapi.readthedocs.io/en/latest/index.html
+autoapi_dirs = ["../gflownet"]
+autoapi_member_order = "alphabetical"
+autoapi_type = "python"
+autodoc_typehints = "description"
+autoapi_keep_files = False
+autoapi_add_objects_to_toctree = False
+autoapi_template_dir = "_autoapi_templates"
+
+# mathjax
+mathjax_path = (
+    "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+)
+mathjax3_config = {
+    "tex": {
+        "inlineMath": [
+            ["$", "$"],
+            ["\\(", "\\)"],
+        ],
+        "processEscapes": True,
+    },
+}
+
+# https://github.com/tox-dev/sphinx-autodoc-typehints
+typehints_fully_qualified = False
+always_document_param_types = True
+typehints_document_rtype = True
+typehints_defaults = "comma"
---
+++ docs/index.rst
@@ -0,0 +1,18 @@
+Welcome to GFlowNet's documentation!
+====================================
+
+.. include:: ../README.md
+   :parser: myst_parser.sphinx_
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   self
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
