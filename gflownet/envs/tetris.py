d9e6367eacc7335ba526457332b8fa92d780c8c6
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-03-25 21:13:02 -0400

    wip: tetris!

---
+++ gflownet/envs/tetris.py
@@ -0,0 +1,373 @@
+"""
+An environment inspired by the game of Tetris.
+"""
+import itertools
+from typing import List, Optional, Tuple
+
+import numpy as np
+import numpy.typing as npt
+import torch
+from torchtyping import TensorType
+
+from gflownet.envs.base import GFlowNetEnv
+from gflownet.utils.tetris.constants import PIECES
+
+
+class Tetris(GFlowNetEnv):
+    """
+    Tetris environment: an environment inspired by the game of tetris. It's not
+    supposed to be a game, but rather a toy environment with an intuitive state and
+    action space.
+
+    The state space is 2D board, with all the combinations of pieces on it.
+
+    The action space is the choice of piece, its rotation and horizontal location
+    where to drop the piece. The action space may be constrained according to needs.
+
+    Attributes
+    ----------
+    width : int
+        Width of the board.
+
+    height : int
+        Height of the board.
+
+    pieces : list
+        Pieces to use, identified by [I, J, L, O, S, T, Z]
+
+    rotations : list
+        Valid rotations, from [0, 90, 180, 270]
+    """
+
+    def __init__(
+        self,
+        width: int = 10,
+        height: int = 20,
+        pieces: List = ["I", "J", "L", "O", "S", "T", "Z"],
+        rotations: List = [0, 90, 180, 270],
+        allow_redundant_rotations: True,
+        **kwargs,
+    ):
+        assert width > 3
+        assert height > 4
+        assert all([p in ["I", "J", "L", "O", "S", "T", "Z"] for p in pieces])
+        assert all([r in [0, 90, 180, 270] for r in rotations])
+        self.width = width
+        self.height = height
+        self.pieces = pieces
+        self.rotations = rotations
+        self.allow_redundant_rotations: allow_redundant_rotations
+        self.rotation2idx = {0: 0, 90: 1, 180: 2, 270: 3} 
+        # Source state: empty board
+        self.source = np.zeros((self.height, self.width), dtype=int)
+        # End-of-sequence action: all -1
+        self.eos = (-1, -1, -1)
+        # Base class init
+        super().__init__(**kwargs)
+
+    def get_action_space(self):
+        """
+        Constructs list with all possible actions, including eos. An action is
+        represented by a tuple of length 3 (piece, rotation, location). The piece is
+        represented by its integer id, the rotation by the integer rotation in degrees
+        and the location by horizontal cell in the board of the left-most part of the
+        piece.
+        """
+        actions = []
+        pieces = []
+        for piece in self.pieces:
+            for rotation in self.pieces:
+                piece = np.rot90(PIECES[piece][1], k=self.rotation2idx[rotation])
+                if piece not in pieces:
+                    pieces.append(piece)
+                else:
+                    continue
+                for location in range(self.width):
+                    
+
+        actions.append(self.eos)
+        return actions
+
+    def get_mask_invalid_actions_forward(
+        self,
+        state: Optional[List] = None,
+        done: Optional[bool] = None,
+    ) -> List:
+        """
+        Returns a list of length the action space with values:
+            - True if the forward action is invalid from the current state.
+            - False otherwise.
+        """
+        if state is None:
+            state = self.state.copy()
+        if done is None:
+            done = self.done
+        if done:
+            return [True for _ in range(self.policy_output_dim)]
+        mask = [False for _ in range(self.policy_output_dim)]
+        for idx, action in enumerate(self.action_space[:-1]):
+            child = state.copy()
+            for dim, incr in enumerate(action):
+                child[dim] += incr
+            if any(el >= self.length for el in child):
+                mask[idx] = True
+        return mask
+
+    def state2oracle(self, state: List = None) -> List:
+        """
+        Prepares a state in "GFlowNet format" for the oracles: a list of length
+        n_dim with values in the range [cell_min, cell_max] for each state.
+
+        See: state2policy()
+
+        Args
+        ----
+        state : list
+            State
+        """
+        if state is None:
+            state = self.state.copy()
+        return (
+            (
+                np.array(self.state2policy(state)).reshape((self.n_dim, self.length))
+                * self.cells[None, :]
+            )
+            .sum(axis=1)
+            .tolist()
+        )
+
+    def statebatch2oracle(
+        self, states: List[List]
+    ) -> TensorType["batch", "state_oracle_dim"]:
+        """
+        Prepares a batch of states in "GFlowNet format" for the oracles: each state is
+        a vector of length n_dim with values in the range [cell_min, cell_max].
+
+        See: statetorch2oracle()
+
+        Args
+        ----
+        state : list
+            State
+        """
+        return self.statetorch2oracle(
+            torch.tensor(states, device=self.device, dtype=self.float)
+        )
+
+    def statetorch2oracle(
+        self, states: TensorType["batch", "state_dim"]
+    ) -> TensorType["batch", "state_oracle_dim"]:
+        """
+        Prepares a batch of states in "GFlowNet format" for the oracles: each state is
+        a vector of length n_dim with values in the range [cell_min, cell_max].
+
+        See: statetorch2policy()
+        """
+        return (
+            self.statetorch2policy(states).reshape(
+                (len(states), self.n_dim, self.length)
+            )
+            * torch.tensor(self.cells[None, :]).to(states)
+        ).sum(axis=2)
+
+    def state2policy(self, state: List = None) -> List:
+        """
+        Transforms the state given as argument (or self.state if None) into a
+        one-hot encoding. The output is a list of len length * n_dim,
+        where each n-th successive block of length elements is a one-hot encoding of
+        the position in the n-th dimension.
+
+        Example:
+          - State, state: [0, 3, 1] (n_dim = 3)
+          - state2policy(state): [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0] (length = 4)
+                              |     0    |      3    |      1    |
+        """
+        if state is None:
+            state = self.state.copy()
+        state_policy = np.zeros(self.length * self.n_dim, dtype=np.float32)
+        state_policy[(np.arange(len(state)) * self.length + state)] = 1
+        return state_policy.tolist()
+
+    def statebatch2policy(self, states: List[List]) -> npt.NDArray[np.float32]:
+        """
+        Transforms a batch of states into a one-hot encoding. The output is a numpy
+        array of shape [n_states, length * n_dim].
+
+        See state2policy().
+        """
+        cols = np.array(states) + np.arange(self.n_dim) * self.length
+        rows = np.repeat(np.arange(len(states)), self.n_dim)
+        state_policy = np.zeros(
+            (len(states), self.length * self.n_dim), dtype=np.float32
+        )
+        state_policy[rows, cols.flatten()] = 1.0
+        return state_policy
+
+    def statetorch2policy(
+        self, states: TensorType["batch", "state_dim"]
+    ) -> TensorType["batch", "policy_output_dim"]:
+        """
+        Transforms a batch of states into a one-hot encoding. The output is a numpy
+        array of shape [n_states, length * n_dim].
+
+        See state2policy().
+        """
+        device = states.device
+        cols = (states + torch.arange(self.n_dim).to(device) * self.length).to(int)
+        rows = torch.repeat_interleave(
+            torch.arange(states.shape[0]).to(device), self.n_dim
+        )
+        state_policy = torch.zeros(
+            (states.shape[0], self.length * self.n_dim), dtype=states.dtype
+        ).to(device)
+        state_policy[rows, cols.flatten()] = 1.0
+        return state_policy
+
+    def policy2state(self, state_policy: List) -> List:
+        """
+        Transforms the one-hot encoding version of a state given as argument
+        into a state (list of the position at each dimension).
+
+        Example:
+          - state_policy: [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0] (length = 4, n_dim = 3)
+                          |     0    |      3    |      1    |
+          - policy2state(state_policy): [0, 3, 1]
+        """
+        return np.where(np.reshape(state_policy, (self.n_dim, self.length)))[1].tolist()
+
+    def readable2state(self, readable, alphabet={}):
+        """
+        Converts a human-readable string representing a state into a state as a list of
+        positions.
+        """
+        return [int(el) for el in readable.strip("[]").split(" ")]
+
+    def state2readable(self, state, alphabet={}):
+        """
+        Converts a state (a list of positions) into a human-readable string
+        representing a state.
+        """
+        return str(state).replace("(", "[").replace(")", "]").replace(",", "")
+
+    def get_parents(
+        self,
+        state: Optional[List] = None,
+        done: Optional[bool] = None,
+        action: Optional[Tuple] = None,
+    ) -> Tuple[List, List]:
+        """
+        Determines all parents and actions that lead to state.
+
+        Args
+        ----
+        state : list
+            Representation of a state, as a list of length length where each element is
+            the position at each dimension.
+
+        done : bool
+            Whether the trajectory is done. If None, done is taken from instance.
+
+        action : None
+            Ignored
+
+        Returns
+        -------
+        parents : list
+            List of parents in state format
+
+        actions : list
+            List of actions that lead to state for each parent in parents
+        """
+        if state is None:
+            state = self.state.copy()
+        if done is None:
+            done = self.done
+        if done:
+            return [state], [self.eos]
+        else:
+            parents = []
+            actions = []
+            for idx, action in enumerate(self.action_space[:-1]):
+                parent = state.copy()
+                for dim, incr in enumerate(action):
+                    if parent[dim] - incr >= 0:
+                        parent[dim] -= incr
+                    else:
+                        break
+                else:
+                    parents.append(parent)
+                    actions.append(action)
+        return parents, actions
+
+    def step(self, action: Tuple[int]) -> Tuple[List[int], Tuple[int], bool]:
+        """
+        Executes step given an action.
+
+        Args
+        ----
+        action : tuple
+            Action to be executed. An action is a tuple int values indicating the
+            dimensions to increment by 1.
+
+        Returns
+        -------
+        self.state : list
+            The sequence after executing the action
+
+        action : tuple
+            Action executed
+
+        valid : bool
+            False, if the action is not allowed for the current state.
+        """
+        # If done, return invalid
+        if self.done:
+            return self.state, action, False
+        # If action not found in action space raise an error
+        if action not in self.action_space:
+            raise ValueError(
+                f"Tried to execute action {action} not present in action space."
+            )
+        else:
+            action_idx = self.action_space.index(action)
+        # If action is in invalid mask, return invalid
+        if self.get_mask_invalid_actions_forward()[action_idx]:
+            return self.state, action, False
+        # TODO: simplify by relying on mask
+        # If only possible action is eos, then force eos
+        # All dimensions are at the maximum length
+        if all([s == self.length - 1 for s in self.state]):
+            self.done = True
+            self.n_actions += 1
+            return self.state, self.eos, True
+        # If action is not eos, then perform action
+        elif action != self.eos:
+            state_next = self.state.copy()
+            for dim, incr in enumerate(action):
+                state_next[dim] += incr
+            if any([s >= self.length for s in state_next]):
+                valid = False
+            else:
+                self.state = state_next
+                valid = True
+                self.n_actions += 1
+            return self.state, action, valid
+        # If action is eos, then perform eos
+        else:
+            self.done = True
+            self.n_actions += 1
+            return self.state, self.eos, True
+
+    def get_max_traj_length(self):
+        return self.n_dim * self.length
+
+    def get_all_terminating_states(self) -> List[List]:
+        all_x = np.int32(
+            list(itertools.product(*[list(range(self.length))] * self.n_dim))
+        )
+        return all_x.tolist()
+
+    def get_uniform_terminating_states(self, n_states: int, seed: int) -> List[List]:
+        rng = np.random.default_rng(seed)
+        states = rng.integers(low=0, high=self.length, size=(n_states, self.n_dim))
+        return states.tolist()
