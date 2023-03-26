18b02a8679e81bbd196b3a1317827b6827090ec9
Alex <alexhg15@gmail.com>
2023-03-26 17:17:41 -0400

    not-tested but all methods done except get_parents

--- gflownet/envs/tetris.py
+++ gflownet/envs/tetris.py
@@ -84,6 +84,9 @@ class Tetris(GFlowNetEnv):
         for piece in self.pieces:
             for rotation in self.rotations:
                 piece_mat = torch.rot90(self.piece2mat(piece), k=self.rot2idx[rotation])
+                import ipdb
+
+                ipdb.set_trace()
                 if self.allow_redundant_rotations or piece not in pieces_mat:
                     pieces_mat.append(piece_mat)
                 else:
@@ -103,8 +106,8 @@ class Tetris(GFlowNetEnv):
         be dropped onto the current and False otherwise.
         """
         if state is None:
-            state = torch.tensor(self.state)
-        board = torch.tensor(state)
+            state = self.state.clone().detach()
+        board = state.clone().detach()
         piece_idx, rotation, location = action
         piece_mat = torch.rot90(
             self.piece2mat(self.pieces(piece_idx)), k=self.rot2idx[rotation]
@@ -129,7 +132,7 @@ class Tetris(GFlowNetEnv):
             - False otherwise.
         """
         if state is None:
-            state = torch.tensor(self.state)
+            state = self.state.clone().detach()
         if done is None:
             done = self.done
         if done:
@@ -139,9 +142,13 @@ class Tetris(GFlowNetEnv):
             _, valid = self._drop_piece_on_board(action)
             if not valid:
                 mask[idx] = True
+        if not self.allow_eos_before_full and not all(mask[:-1]):
+            mask[-1] = True
         return mask
 
-    def state2oracle(self, state: Optional[TensorType["height", "width"]] = None) -> TensorType["height", "width"]:
+    def state2oracle(
+        self, state: Optional[TensorType["height", "width"]] = None
+    ) -> TensorType["height", "width"]:
         """
         Prepares a state in "GFlowNet format" for the oracles: simply converts non-zero
         (non-empty) cells into 1s.
@@ -149,126 +156,90 @@ class Tetris(GFlowNetEnv):
         Args
         ----
         state : tensor
-            State
         """
         if state is None:
-            state = torch.tensor(self.state)
-        state_oracle = torch.tensor(state)
+            state = self.state.clone().detach()
+        state_oracle = state.clone().detach()
         state_oracle[state_oracle != 0] = 1
         return state_oracle
 
     def statebatch2oracle(
-        self, states: List[List]
+        self, states: List[TensorType["height", "width"]]
     ) -> TensorType["batch", "state_oracle_dim"]:
         """
-        Prepares a batch of states in "GFlowNet format" for the oracles: each state is
-        a vector of length n_dim with values in the range [cell_min, cell_max].
-
-        See: statetorch2oracle()
+        Prepares a batch of states in "GFlowNet format" for the oracles: simply
+        converts non-zero (non-empty) cells into 1s.
 
         Args
         ----
         state : list
-            State
         """
-        return self.statetorch2oracle(
-            torch.tensor(states, device=self.device, dtype=self.float)
-        )
+        states = torch.stack(states)
+        states[states != 0] = 1
+        return states
 
     def statetorch2oracle(
-        self, states: TensorType["batch", "state_dim"]
+        self, states: TensorType["height", "width", "batch"]
+    ) -> TensorType["height", "width", "batch"]:
+        """
+        Prepares a batch of states in "GFlowNet format" for the oracles: : simply
+        converts non-zero (non-empty) cells into 1s.
+        """
+        states[states != 0] = 1
+        return states
+
+    def state2policy(
+        self, state: Optional[TensorType["height", "width"]] = None
+    ) -> TensorType["height", "width"]:
+        """
+        Prepares a state in "GFlowNet format" for the policy model.
+
+        See: state2oracle()
+        """
+        return self.state2oracle(state)
+
+    def statebatch2policy(
+        self, states: List[TensorType["height", "width"]]
     ) -> TensorType["batch", "state_oracle_dim"]:
         """
-        Prepares a batch of states in "GFlowNet format" for the oracles: each state is
-        a vector of length n_dim with values in the range [cell_min, cell_max].
+        Prepares a batch of states in "GFlowNet format" for the policy model.
 
-        See: statetorch2policy()
+        See statebatch2oracle().
         """
-        return (
-            self.statetorch2policy(states).reshape(
-                (len(states), self.n_dim, self.length)
-            )
-            * torch.tensor(self.cells[None, :]).to(states)
-        ).sum(axis=2)
-
-    def state2policy(self, state: List = None) -> List:
-        """
-        Transforms the state given as argument (or self.state if None) into a
-        one-hot encoding. The output is a list of len length * n_dim,
-        where each n-th successive block of length elements is a one-hot encoding of
-        the position in the n-th dimension.
-
-        Example:
-          - State, state: [0, 3, 1] (n_dim = 3)
-          - state2policy(state): [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0] (length = 4)
-                              |     0    |      3    |      1    |
-        """
-        if state is None:
-            state = self.state.copy()
-        state_policy = np.zeros(self.length * self.n_dim, dtype=np.float32)
-        state_policy[(np.arange(len(state)) * self.length + state)] = 1
-        return state_policy.tolist()
-
-    def statebatch2policy(self, states: List[List]) -> npt.NDArray[np.float32]:
-        """
-        Transforms a batch of states into a one-hot encoding. The output is a numpy
-        array of shape [n_states, length * n_dim].
-
-        See state2policy().
-        """
-        cols = np.array(states) + np.arange(self.n_dim) * self.length
-        rows = np.repeat(np.arange(len(states)), self.n_dim)
-        state_policy = np.zeros(
-            (len(states), self.length * self.n_dim), dtype=np.float32
-        )
-        state_policy[rows, cols.flatten()] = 1.0
-        return state_policy
+        return self.statebatch2oracle(states)
 
     def statetorch2policy(
-        self, states: TensorType["batch", "state_dim"]
-    ) -> TensorType["batch", "policy_output_dim"]:
+        self, states: TensorType["height", "width", "batch"]
+    ) -> TensorType["height", "width", "batch"]:
         """
-        Transforms a batch of states into a one-hot encoding. The output is a numpy
-        array of shape [n_states, length * n_dim].
+        Prepares a batch of states in "GFlowNet format" for the policy model.
 
-        See state2policy().
+        See statetorch2oracle().
         """
-        device = states.device
-        cols = (states + torch.arange(self.n_dim).to(device) * self.length).to(int)
-        rows = torch.repeat_interleave(
-            torch.arange(states.shape[0]).to(device), self.n_dim
-        )
-        state_policy = torch.zeros(
-            (states.shape[0], self.length * self.n_dim), dtype=states.dtype
-        ).to(device)
-        state_policy[rows, cols.flatten()] = 1.0
-        return state_policy
+        return self.statetorch2oracle(states)
 
-    def policy2state(self, state_policy: List) -> List:
+    def state2readable(self, state):
         """
-        Transforms the one-hot encoding version of a state given as argument
-        into a state (list of the position at each dimension).
-
-        Example:
-          - state_policy: [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0] (length = 4, n_dim = 3)
-                          |     0    |      3    |      1    |
-          - policy2state(state_policy): [0, 3, 1]
+        Converts a state (board) into a human-friendly string.
         """
-        return np.where(np.reshape(state_policy, (self.n_dim, self.length)))[1].tolist()
+        readable = str(state.numpy())
+        readable = readable.replace("[[", "[").replace("]]", "]").replace("\n ", "\n")
+        return readable
 
     def readable2state(self, readable, alphabet={}):
         """
         Converts a human-readable string representing a state into a state as a list of
         positions.
         """
-        return [int(el) for el in readable.strip("[]").split(" ")]
-
-    def state2readable(self, state, alphabet={}):
-        """
-        Converts a state (a list of positions) into a human-readable string
-        representing a state.
-        """
-        return str(state).replace("(", "[").replace(")", "]").replace(",", "")
+        state = []
+        rows = readable.split("\n")
+        for row in rows:
+            state.append(
+                torch.tensor(
+                    [int(el) for el in row.strip("[]").split(" ")], dtype=torch.uint8
+                )
+            )
+        return torch.stack(state)
 
     def get_parents(
         self,
@@ -352,44 +323,22 @@ class Tetris(GFlowNetEnv):
         else:
             action_idx = self.action_space.index(action)
         # If action is in invalid mask, return invalid
-        if self.get_mask_invalid_actions_forward()[action_idx]:
+        mask_invalid = self.get_mask_invalid_actions_forward()
+        if mask_invalid[action_idx]:
             return self.state, action, False
-        # TODO: simplify by relying on mask
         # If only possible action is eos, then force eos
-        # All dimensions are at the maximum length
-        if all([s == self.length - 1 for s in self.state]):
+        if all(mask_invalid[:-1]):
             self.done = True
             self.n_actions += 1
             return self.state, self.eos, True
         # If action is not eos, then perform action
-        elif action != self.eos:
-            state_next = self.state.copy()
-            for dim, incr in enumerate(action):
-                state_next[dim] += incr
-            if any([s >= self.length for s in state_next]):
-                valid = False
-            else:
+        else:
+            next_state, valid = self._drop_piece_on_board(action)
+            if valid:
                 self.state = state_next
-                valid = True
                 self.n_actions += 1
             return self.state, action, valid
-        # If action is eos, then perform eos
-        else:
-            self.done = True
-            self.n_actions += 1
-            return self.state, self.eos, True
 
     # TODO
     def get_max_traj_length(self):
         return 1e9
-
-    def get_all_terminating_states(self) -> List[List]:
-        all_x = np.int32(
-            list(itertools.product(*[list(range(self.length))] * self.n_dim))
-        )
-        return all_x.tolist()
-
-    def get_uniform_terminating_states(self, n_states: int, seed: int) -> List[List]:
-        rng = np.random.default_rng(seed)
-        states = rng.integers(low=0, high=self.length, size=(n_states, self.n_dim))
-        return states.tolist()
