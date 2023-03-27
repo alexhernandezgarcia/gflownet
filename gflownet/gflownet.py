31da86abd282483a1364024ba448365bad55e0a2
Alex <alexhg15@gmail.com>
2023-03-27 00:45:23 -0400

    wip: copy paste mlp to code cnn

--- gflownet/gflownet.py
+++ gflownet/gflownet.py
@@ -1001,6 +1001,9 @@ class Policy:
         elif self.type == "mlp":
             self.model = self.make_mlp(nn.LeakyReLU())
             self.is_model = True
+        elif self.type == "cnn":
+            self.model = self.make_cnn(nn.LeakyReLU())
+            self.is_model = True
         else:
             raise "Policy model type not defined"
         if self.is_model:
@@ -1023,6 +1026,51 @@ class Policy:
         """
         if self.shared_weights == True and self.base is not None:
             mlp = nn.Sequential(
+                self.base.model[:-1],
+                nn.Linear(
+                    self.base.model[-1].in_features, self.base.model[-1].out_features
+                ),
+            )
+            return mlp
+        elif self.shared_weights == False:
+            layers_dim = (
+                [self.state_dim] + [self.n_hid] * self.n_layers + [(self.output_dim)]
+            )
+            mlp = nn.Sequential(
+                *(
+                    sum(
+                        [
+                            [nn.Linear(idim, odim)]
+                            + ([activation] if n < len(layers_dim) - 2 else [])
+                            for n, (idim, odim) in enumerate(
+                                zip(layers_dim, layers_dim[1:])
+                            )
+                        ],
+                        [],
+                    )
+                    + self.tail
+                )
+            )
+            return mlp
+        else:
+            raise ValueError(
+                "Base Model must be provided when shared_weights is set to True"
+            )
+
+    def make_cnn(self, activation):
+        """
+        Defines a CNN with no top layer activation
+        If share_weight == True,
+            baseModel (the model with which weights are to be shared) must be provided
+        Args
+        ----
+        layers_dim : list
+            Dimensionality of each layer
+        activation : Activation
+            Activation function
+        """
+        if self.shared_weights == True and self.base is not None:
+            cnn = nn.Sequential(
                 self.base.model[:-1],
                 nn.Linear(
                     self.base.model[-1].in_features, self.base.model[-1].out_features
