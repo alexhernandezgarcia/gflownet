05d2a8218984b797160fa5eb9a75dc631ba3aafe
michalkoziarski <michal.koziarski@gmail.com>
2023-06-19 16:06:22 -0400

    added remaining head models

--- gflownet/policy/tree.py
+++ gflownet/policy/tree.py
@@ -3,11 +3,13 @@ import torch
 import torch.nn.functional as F
 import torch_geometric
 
+from torch_geometric.nn import global_mean_pool
+
 
 class Backbone(torch.nn.Module):
     def __init__(
         self,
-        n_layers: int = 5,
+        n_layers: int = 3,
         hidden_dim: int = 64,
         input_dim: int = 4,
         layer: str = "GCNConv",
@@ -79,3 +81,144 @@ class LeafSelectionHead(torch.nn.Module):
         x = x.masked_fill(mask, -np.inf)
 
         return F.softmax(x, dim=0)
+
+
+class FeatureSelectionHead(torch.nn.Module):
+    def __init__(
+        self,
+        backbone: torch.nn.Module,
+        output_dim: int,
+        n_layers: int = 2,
+        input_dim: int = 128,
+        hidden_dim: int = 64,
+        activation: str = "LeakyReLU",
+        dropout: float = 0.5,
+    ):
+        super().__init__()
+
+        activation = getattr(torch.nn, activation)
+
+        layers = []
+        for i in range(n_layers):
+            layers.append(
+                torch.nn.Linear(
+                    input_dim if i == 0 else hidden_dim,
+                    output_dim if i == n_layers - 1 else hidden_dim,
+                ),
+            )
+            if i < n_layers - 1:
+                layers.append(activation())
+                if dropout > 0:
+                    layers.append(torch.nn.Dropout(p=dropout))
+        layers.append(torch.nn.Softmax(dim=1))
+
+        self.backbone = backbone
+        self.model = torch.nn.Sequential(*layers)
+
+    def forward(
+        self, data: torch_geometric.data.Data, node_index: torch.Tensor
+    ) -> torch.Tensor:
+        x, edge_index, batch = (data.x, data.edge_index, data.batch)
+        x = self.backbone(data)
+        x_pool = global_mean_pool(x, batch)
+        x_node = x[node_index, :]
+        x = torch.cat([x_pool, x_node], dim=1)
+        x = self.model(x)
+
+        return x
+
+
+class ThresholdSelectionHead(torch.nn.Module):
+    def __init__(
+        self,
+        backbone: torch.nn.Module,
+        input_dim: int,
+        output_dim: int,
+        n_layers: int = 2,
+        hidden_dim: int = 64,
+        activation: str = "LeakyReLU",
+        dropout: float = 0.5,
+    ):
+        super().__init__()
+
+        activation = getattr(torch.nn, activation)
+
+        layers = []
+        for i in range(n_layers):
+            layers.append(
+                torch.nn.Linear(
+                    input_dim if i == 0 else hidden_dim,
+                    output_dim if i == n_layers - 1 else hidden_dim,
+                ),
+            )
+            if i < n_layers - 1:
+                layers.append(activation())
+                if dropout > 0:
+                    layers.append(torch.nn.Dropout(p=dropout))
+        layers.append(torch.nn.ReLU())
+
+        self.backbone = backbone
+        self.model = torch.nn.Sequential(*layers)
+
+    def forward(
+        self,
+        data: torch_geometric.data.Data,
+        node_index: torch.Tensor,
+        feature_index: torch.Tensor,
+    ) -> torch.Tensor:
+        x, edge_index, batch = (data.x, data.edge_index, data.batch)
+        x = self.backbone(data)
+        x_pool = global_mean_pool(x, batch)
+        x_node = x[node_index, :]
+        x = torch.cat([x_pool, x_node, feature_index], dim=1)
+        x = self.model(x)
+
+        return x
+
+
+class SignSelectionHead(torch.nn.Module):
+    def __init__(
+        self,
+        backbone: torch.nn.Module,
+        input_dim: int,
+        n_layers: int = 2,
+        hidden_dim: int = 64,
+        activation: str = "LeakyReLU",
+        dropout: float = 0.5,
+    ):
+        super().__init__()
+
+        activation = getattr(torch.nn, activation)
+
+        layers = []
+        for i in range(n_layers):
+            layers.append(
+                torch.nn.Linear(
+                    input_dim if i == 0 else hidden_dim,
+                    1 if i == n_layers - 1 else hidden_dim,
+                ),
+            )
+            if i < n_layers - 1:
+                layers.append(activation())
+                if dropout > 0:
+                    layers.append(torch.nn.Dropout(p=dropout))
+        layers.append(torch.nn.Sigmoid())
+
+        self.backbone = backbone
+        self.model = torch.nn.Sequential(*layers)
+
+    def forward(
+        self,
+        data: torch_geometric.data.Data,
+        node_index: torch.Tensor,
+        feature_index: torch.Tensor,
+        threshold: torch.Tensor,
+    ) -> torch.Tensor:
+        x, edge_index, batch = (data.x, data.edge_index, data.batch)
+        x = self.backbone(data)
+        x_pool = global_mean_pool(x, batch)
+        x_node = x[node_index, :]
+        x = torch.cat([x_pool, x_node, feature_index, threshold], dim=1)
+        x = self.model(x)
+
+        return x
