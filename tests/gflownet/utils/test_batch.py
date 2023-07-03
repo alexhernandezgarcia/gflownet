b5e7e0ac7dd903a7875c7bb0b6013a6e2c5350e6
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-07-03 09:59:11 -0400

    test get_parents

--- tests/gflownet/utils/test_batch.py
+++ tests/gflownet/utils/test_batch.py
@@ -24,6 +24,7 @@ def test__len__returnszero_at_init(batch_tb, batch_fm):
     assert len(batch_tb) == 0
     assert len(batch_fm) == 0
 
+
 @pytest.mark.repeat(10)
 def test__add_to_batch__single_env_adds_expected(grid2d, batch_tb):
     grid2d = grid2d.reset()
@@ -41,20 +42,22 @@ def test__add_to_batch__single_env_adds_expected(grid2d, batch_tb):
         assert batch_tb.done[-1] == grid2d.done
         assert batch_tb.n_actions[-1] == grid2d.n_actions
 
-# @pytest.mark.repeat(10)
-# def test__get_parents__single_env_returns_expected(grid2d, batch_tb):
-#     grid2d = grid2d.reset()
-#     parents = []
-#     while not grid2d.done:
-#         parent = grid2d.state
-#         # Sample random action
-#         _, action, valid = grid2d.step_random()
-#         # Add to batch
-#         batch_tb.add_to_batch([grid2d], [action], [valid])
-#         if valid:
-#             parents.append(parent)
-#     parents_batch = batch_tb.get_parents()
-#     assert parents_batch == parents
+
+@pytest.mark.repeat(10)
+def test__get_parents__single_env_returns_expected(grid2d, batch_tb):
+    grid2d = grid2d.reset()
+    parents = []
+    while not grid2d.done:
+        parent = grid2d.state
+        # Sample random action
+        _, action, valid = grid2d.step_random()
+        # Add to batch
+        batch_tb.add_to_batch([grid2d], [action], [valid])
+        if valid:
+            parents.append(parent)
+    parents_batch = batch_tb.get_parents()
+    assert parents_batch == parents
+
 
 @pytest.mark.repeat(10)
 def test__get_states__single_env_returns_expected(grid2d, batch_tb):
@@ -69,6 +72,7 @@ def test__get_states__single_env_returns_expected(grid2d, batch_tb):
             states.append(state)
     states_batch = batch_tb.get_states()
     assert states_batch == states
+
 
 @pytest.mark.parametrize(
     "action, state_expected",
