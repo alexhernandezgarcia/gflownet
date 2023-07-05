5aeeed892a0f07f3a07899b6d738e1f0d4529bda
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-07-05 18:17:16 -0400

    add tetris tests to batch tests

--- tests/gflownet/utils/test_batch.py
+++ tests/gflownet/utils/test_batch.py
@@ -2,8 +2,9 @@ import pytest
 import torch
 
 from gflownet.envs.grid import Grid
+from gflownet.envs.tetris import Tetris
 from gflownet.proxy.corners import Corners
-from gflownet.proxy.uniform import Uniform
+from gflownet.proxy.tetris import Tetris as TetrisScore
 from gflownet.utils.batch import Batch
 from gflownet.utils.common import (
     concat_items,
@@ -17,13 +18,8 @@ from gflownet.utils.common import (
 
 
 @pytest.fixture
-def batch_tb():
-    return Batch(loss="trajectorybalance")
-
-
-@pytest.fixture
-def batch_fm():
-    return Batch(loss="flowmatch")
+def batch():
+    return Batch()
 
 
 @pytest.fixture
@@ -31,205 +27,231 @@ def grid2d():
     return Grid(n_dim=2, length=3, cell_min=-1.0, cell_max=1.0)
 
 
+@pytest.fixture
+def tetris6x4():
+    return Tetris(width=4, height=6)
+
+
 @pytest.fixture()
-def uniform_proxy():
-    return Uniform()
+def corners():
+    return Corners(device="cpu", float_precision=32, mu=0.75, sigma=0.05)
 
 
-def test__len__returnszero_at_init(batch_tb, batch_fm):
-    assert len(batch_tb) == 0
-    assert len(batch_fm) == 0
+@pytest.fixture()
+def tetris_score():
+    return TetrisScore(device="cpu", float_precision=32, normalize=False)
+
+
+def test__len__returnszero_at_init(batch):
+    assert len(batch) == 0
 
 
 @pytest.mark.repeat(10)
-def test__add_to_batch__single_env_adds_expected(grid2d, batch_tb):
-    grid2d = grid2d.reset()
-    while not grid2d.done:
+@pytest.mark.parametrize("env", ["grid2d", "tetris6x4"])
+def test__add_to_batch__single_env_adds_expected(env, batch, request):
+    env = request.getfixturevalue(env)
+    env = env.reset()
+    while not env.done:
         # Sample random action
-        state, action, valid = grid2d.step_random()
+        state, action, valid = env.step_random()
         # Add to batch
-        batch_tb.add_to_batch([grid2d], [action], [valid])
+        batch.add_to_batch([env], [action], [valid])
         if valid is False:
             continue
         # Checks
-        assert batch_tb.env_ids[-1] == grid2d.id
-        assert batch_tb.states[-1] == state
-        assert batch_tb.actions[-1] == action
-        assert batch_tb.done[-1] == grid2d.done
-        assert batch_tb.n_actions[-1] == grid2d.n_actions
+        assert batch.env_ids[-1] == env.id
+        if torch.is_tensor(state):
+            assert torch.equal(batch.states[-1], state)
+        else:
+            assert batch.states[-1] == state
+        assert batch.actions[-1] == action
+        assert batch.done[-1] == env.done
+        assert batch.n_actions[-1] == env.n_actions
 
 
 @pytest.mark.repeat(10)
-def test__get_states__single_env_returns_expected(grid2d, batch_tb):
-    grid2d = grid2d.reset()
+@pytest.mark.parametrize("env", ["grid2d", "tetris6x4"])
+def test__get_states__single_env_returns_expected(env, batch, request):
+    env = request.getfixturevalue(env)
+    env = env.reset()
     states = []
-    while not grid2d.done:
+    while not env.done:
         # Sample random action
-        state, action, valid = grid2d.step_random()
+        state, action, valid = env.step_random()
         # Add to batch
-        batch_tb.add_to_batch([grid2d], [action], [valid])
+        batch.add_to_batch([env], [action], [valid])
         if valid:
             states.append(state)
-    states_batch = batch_tb.get_states()
-    states_policy_batch = batch_tb.get_states(policy=True)
-    assert states_batch == states
+    states_batch = batch.get_states()
+    states_policy_batch = batch.get_states(policy=True)
+    if torch.is_tensor(states[0]):
+        assert torch.equal(torch.stack(states_batch), torch.stack(states))
+    else:
+        assert states_batch == states
     assert torch.equal(
         states_policy_batch,
         tfloat(
-            grid2d.statebatch2policy(states),
-            device=batch_tb.device,
-            float_type=batch_tb.float,
+            env.statebatch2policy(states),
+            device=batch.device,
+            float_type=batch.float,
         ),
     )
 
 
 @pytest.mark.repeat(10)
-def test__get_parents__single_env_returns_expected(grid2d, batch_tb):
-    grid2d = grid2d.reset()
+@pytest.mark.parametrize("env", ["grid2d", "tetris6x4"])
+def test__get_parents__single_env_returns_expected(env, batch, request):
+    env = request.getfixturevalue(env)
+    env = env.reset()
     parents = []
-    while not grid2d.done:
-        parent = grid2d.state
+    while not env.done:
+        parent = env.state
         # Sample random action
-        _, action, valid = grid2d.step_random()
+        _, action, valid = env.step_random()
         # Add to batch
-        batch_tb.add_to_batch([grid2d], [action], [valid])
+        batch.add_to_batch([env], [action], [valid])
         if valid:
             parents.append(parent)
-    parents_batch = batch_tb.get_parents()
-    parents_policy_batch = batch_tb.get_parents(policy=True)
-    assert parents_batch == parents
+    parents_batch = batch.get_parents()
+    parents_policy_batch = batch.get_parents(policy=True)
+    if torch.is_tensor(parents[0]):
+        assert torch.equal(torch.stack(parents_batch), torch.stack(parents))
+    else:
+        assert parents_batch == parents
     assert torch.equal(
         parents_policy_batch,
         tfloat(
-            grid2d.statebatch2policy(parents),
-            device=batch_tb.device,
-            float_type=batch_tb.float,
+            env.statebatch2policy(parents),
+            device=batch.device,
+            float_type=batch.float,
         ),
     )
 
 
 @pytest.mark.repeat(10)
-def test__get_parents_all__single_env_returns_expected(grid2d, batch_tb):
-    grid2d = grid2d.reset()
+@pytest.mark.parametrize("env", ["grid2d", "tetris6x4"])
+def test__get_parents_all__single_env_returns_expected(env, batch, request):
+    env = request.getfixturevalue(env)
+    env = env.reset()
     parents_all = []
     parents_all_a = []
-    while not grid2d.done:
+    while not env.done:
         # Sample random action
-        _, action, valid = grid2d.step_random()
+        _, action, valid = env.step_random()
         # Add to batch
-        batch_tb.add_to_batch([grid2d], [action], [valid])
+        batch.add_to_batch([env], [action], [valid])
         if valid:
-            parents, parents_a = grid2d.get_parents()
+            parents, parents_a = env.get_parents()
             parents_all.extend(parents)
             parents_all_a.extend(parents_a)
-    parents_all_batch, parents_all_a_batch, _ = batch_tb.get_parents_all()
-    parents_all_policy_batch, _, _ = batch_tb.get_parents_all(policy=True)
-    assert parents_all_batch == parents_all
+    parents_all_batch, parents_all_a_batch, _ = batch.get_parents_all()
+    parents_all_policy_batch, _, _ = batch.get_parents_all(policy=True)
+    if torch.is_tensor(parents_all[0]):
+        assert torch.equal(torch.stack(parents_all_batch), torch.stack(parents_all))
+    else:
+        assert parents_all_batch == parents_all
     assert torch.equal(
         parents_all_a_batch,
         tfloat(
             parents_all_a,
-            device=batch_tb.device,
-            float_type=batch_tb.float,
+            device=batch.device,
+            float_type=batch.float,
         ),
     )
     assert torch.equal(
         parents_all_policy_batch,
         tfloat(
-            grid2d.statebatch2policy(parents_all),
-            device=batch_tb.device,
-            float_type=batch_tb.float,
+            env.statebatch2policy(parents_all),
+            device=batch.device,
+            float_type=batch.float,
         ),
     )
 
 
 @pytest.mark.repeat(10)
-def test__get_masks_forward__single_env_returns_expected(grid2d, batch_tb):
-    grid2d = grid2d.reset()
+@pytest.mark.parametrize("env", ["grid2d", "tetris6x4"])
+def test__get_masks_forward__single_env_returns_expected(env, batch, request):
+    env = request.getfixturevalue(env)
+    env = env.reset()
     masks_forward = []
-    while not grid2d.done:
-        parent = grid2d.state
+    while not env.done:
+        parent = env.state
         # Sample random action
-        _, action, valid = grid2d.step_random()
+        _, action, valid = env.step_random()
         # Add to batch
-        batch_tb.add_to_batch([grid2d], [action], [valid])
+        batch.add_to_batch([env], [action], [valid])
         if valid:
-            masks_forward.append(grid2d.get_mask_invalid_actions_forward())
-    masks_forward_batch = batch_tb.get_masks_forward()
-    assert torch.equal(
-        masks_forward_batch, tbool(masks_forward, device=batch_tb.device)
-    )
+            masks_forward.append(env.get_mask_invalid_actions_forward())
+    masks_forward_batch = batch.get_masks_forward()
+    assert torch.equal(masks_forward_batch, tbool(masks_forward, device=batch.device))
 
 
 @pytest.mark.repeat(10)
-def test__get_masks_backward__single_env_returns_expected(grid2d, batch_tb):
-    grid2d = grid2d.reset()
+@pytest.mark.parametrize("env", ["grid2d", "tetris6x4"])
+def test__get_masks_backward__single_env_returns_expected(env, batch, request):
+    env = request.getfixturevalue(env)
+    env = env.reset()
     masks_backward = []
-    while not grid2d.done:
-        parent = grid2d.state
+    while not env.done:
+        parent = env.state
         # Sample random action
-        _, action, valid = grid2d.step_random()
+        _, action, valid = env.step_random()
         # Add to batch
-        batch_tb.add_to_batch([grid2d], [action], [valid])
+        batch.add_to_batch([env], [action], [valid])
         if valid:
-            masks_backward.append(grid2d.get_mask_invalid_actions_backward())
-    masks_backward_batch = batch_tb.get_masks_backward()
-    assert torch.equal(
-        masks_backward_batch, tbool(masks_backward, device=batch_tb.device)
-    )
+            masks_backward.append(env.get_mask_invalid_actions_backward())
+    masks_backward_batch = batch.get_masks_backward()
+    assert torch.equal(masks_backward_batch, tbool(masks_backward, device=batch.device))
 
 
 @pytest.mark.repeat(10)
-def test__get_rewards__single_env_returns_expected(grid2d, batch_tb):
-    grid2d = grid2d.reset()
-    proxy = Corners(
-        device=grid2d.device, float_precision=grid2d.float, mu=0.75, sigma=0.05
-    )
-    grid2d.proxy = proxy
-    grid2d.setup_proxy()
+@pytest.mark.parametrize(
+    "env, proxy", [("grid2d", "corners"), ("tetris6x4", "tetris_score")]
+)
+def test__get_rewards__single_env_returns_expected(env, proxy, batch, request):
+    env = request.getfixturevalue(env)
+    proxy = request.getfixturevalue(proxy)
+    env = env.reset()
+    env.proxy = proxy
+    env.setup_proxy()
 
     rewards = []
-    while not grid2d.done:
-        parent = grid2d.state
+    while not env.done:
+        parent = env.state
         # Sample random action
-        _, action, valid = grid2d.step_random()
+        _, action, valid = env.step_random()
         # Add to batch
-        batch_tb.add_to_batch([grid2d], [action], [valid])
+        batch.add_to_batch([env], [action], [valid])
         if valid:
-            rewards.append(grid2d.reward())
-    rewards_batch = batch_tb.get_rewards()
+            rewards.append(env.reward())
+    rewards_batch = batch.get_rewards()
     rewards = torch.stack(rewards)
     assert torch.equal(
         rewards_batch,
-        tfloat(rewards, device=batch_tb.device, float_type=batch_tb.float),
+        tfloat(rewards, device=batch.device, float_type=batch.float),
     ), (rewards, rewards_batch)
 
 
-@pytest.mark.parametrize(
-    "action, state_expected",
-    [
-        (
-            (1, 0),
-            [1, 0],
-        ),
-        (
-            (0, 1),
-            [0, 1],
-        ),
-    ],
-)
-def test__add_to_batch__minimal_grid2d_returns_expected(
-    batch_tb, batch_fm, grid2d, action, state_expected
-):
-    state, action_step, valid = grid2d.step(action)
-    assert state == state_expected
-    assert action_step == action
-    assert valid
-    # TB
-    batch_tb.add_to_batch([grid2d], [action], [valid])
-    assert batch_tb.states == [state_expected]
-    assert batch_tb.actions == [action]
-    # FM
-    batch_fm.add_to_batch([grid2d], [action], [valid])
-    assert batch_fm.states == [state_expected]
-    assert batch_fm.actions == [action]
+# @pytest.mark.parametrize(
+#     "action, state_expected",
+#     [
+#         (
+#             (1, 0),
+#             [1, 0],
+#         ),
+#         (
+#             (0, 1),
+#             [0, 1],
+#         ),
+#     ],
+# )
+# def test__add_to_batch__minimal_env_returns_expected(
+#     batch, grid2d, action, state_expected
+# ):
+#     state, action_step, valid = grid2d.step(action)
+#     assert state == state_expected
+#     assert action_step == action
+#     assert valid
+#     batch.add_to_batch([grid2d], [action], [valid])
+#     assert batch.states == [state_expected]
+#     assert batch.actions == [action]
