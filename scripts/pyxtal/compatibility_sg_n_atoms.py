8933107c4e43cbacd5f51805946167f41e49ad01
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-07-15 15:41:50 -0400

    add description to scripts and add get_n_compatible_for_sg.py to repo

--- scripts/pyxtal/compatibility_sg_n_atoms.py
+++ scripts/pyxtal/compatibility_sg_n_atoms.py
@@ -1,3 +1,8 @@
+"""
+Calculates the compatibility between space groups and stoichiometries for all the
+combinations spanned by the N_SYMMETRY_GROUPS, N_SPECIES and MAX_N_ATOMS. The results
+are printed to stdout.
+"""
 import itertools
 import time
 
@@ -6,8 +11,8 @@ from pyxtal.symmetry import Group
 from tqdm import tqdm
 
 N_SYMMETRY_GROUPS = 230
-N_SPECIES = 5
-MAX_N_ATOMS = 10
+N_SPECIES = 3
+MAX_N_ATOMS = 20
 
 n_compatible = 0
 n_not_compatible = 0
@@ -15,7 +20,8 @@ times = []
 for idx in tqdm(range(1, N_SYMMETRY_GROUPS + 1)):
     sg = Group(idx)
     times_sg = []
-    for n_atoms in itertools.product(range(1, MAX_N_ATOMS + 1), repeat=N_SPECIES):
+    for n_atoms_withzeros in itertools.product(range(0, MAX_N_ATOMS + 1), repeat=N_SPECIES):
+        n_atoms = [n for n in n_atoms_withzeros if n > 0]
         time0 = time.time()
         is_compatible, _ = sg.check_compatible(list(n_atoms))
         times_sg.append(time.time() - time0)
