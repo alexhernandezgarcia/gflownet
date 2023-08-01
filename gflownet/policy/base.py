c8c04f8418269aa0f876d084571a890f786192da
michalkoziarski <michal.koziarski@gmail.com>
2023-08-01 15:47:05 -0400

    device and precision setting in policy

--- gflownet/policy/base.py
+++ gflownet/policy/base.py
@@ -2,12 +2,14 @@ import torch
 from torch import nn
 from omegaconf import OmegaConf
 
+from gflownet.utils.common import set_device, set_float_precision
+
 
 class Policy:
     def __init__(self, config, env, device, float_precision, base=None):
         # Device and float precision
-        self.device = device
-        self.float = float_precision
+        self.device = set_device(device)
+        self.float = set_float_precision(float_precision)
         # Input and output dimensions
         self.state_dim = env.policy_input_dim
         self.fixed_output = torch.tensor(env.fixed_policy_output).to(
