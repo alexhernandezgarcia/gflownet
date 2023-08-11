2e44acc676e76ca59e6eefac7b339ba7d3560faa
michalkoziarski <michal.koziarski@gmail.com>
2023-08-11 16:34:01 -0400

    EGNN-based policy

--- gflownet/envs/conformers/conformer.py
+++ gflownet/envs/conformers/conformer.py
@@ -8,6 +8,8 @@ from rdkit.Chem import AllChem
 from torchtyping import TensorType
 
 from gflownet.envs.ctorus import ContinuousTorus
+from gflownet.utils.molecule.constants import ad_atom_types
+from gflownet.utils.molecule.featurizer import MolDGLFeaturizer
 from gflownet.utils.molecule.rdkit_conformer import RDKitConformer
 from gflownet.utils.molecule.rotatable_bonds import find_rotor_from_smile
 
@@ -23,6 +25,7 @@ class Conformer(ContinuousTorus):
         smiles: str,
         n_torsion_angles: Optional[int] = 2,
         torsion_indices: Optional[List[int]] = None,
+        policy_type: str = "mlp",
         **kwargs,
     ):
         if torsion_indices is None:
@@ -40,10 +43,18 @@ class Conformer(ContinuousTorus):
         # Conversions
         self.statebatch2oracle = self.statebatch2proxy
         self.statetorch2oracle = self.statetorch2proxy
+        if policy_type == "gnn":
+            self.statebatch2policy = self.statebatch2policy_gnn
+        elif policy_type != "mlp":
+            raise ValueError(
+                f"Unrecognized policy_type = {policy_type}, expected either 'mlp' or 'gnn'."
+            )
 
         super().__init__(n_dim=len(self.conformer.freely_rotatable_tas), **kwargs)
 
         self.sync_conformer_with_state()
+
+        self.graph = MolDGLFeaturizer(ad_atom_types).mol2dgl(self.conformer.rdk_mol)
 
     @staticmethod
     def _get_positions(smiles: str) -> npt.NDArray:
@@ -87,6 +98,24 @@ class Conformer(ContinuousTorus):
 
     def statetorch2proxy(self, states: TensorType["batch", "state_dim"]) -> npt.NDArray:
         return self.statebatch2proxy(states.cpu().numpy())
+
+    def statebatch2policy_gnn(self, states: List[List]) -> npt.NDArray[np.float32]:
+        """
+        Returns an array of GNN-format policy inputs with dimensionality
+        (n_states, n_atoms, 4), in which the first three columns encode atom positions,
+        and the last column encodes current timestep.
+        """
+        policy_input = []
+        for state in states:
+            conformer = self.sync_conformer_with_state(state)
+            positions = conformer.get_atom_positions()
+            policy_input.append(
+                np.concatenate(
+                    [positions, np.full((positions.shape[0], 1), state[-1])],
+                    axis=1,
+                )
+            )
+        return np.array(policy_input)
 
     def statebatch2kde(self, states: List[List]) -> npt.NDArray[np.float32]:
         return np.array(states)[:, :-1]
