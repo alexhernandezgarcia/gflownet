ddf8bf6cf958e1062649476b4e5fd7820d28776d
michalkoziarski <michal.koziarski@gmail.com>
2023-08-24 19:16:30 -0400

    exponential prior

--- gflownet/proxy/tree.py
+++ gflownet/proxy/tree.py
@@ -4,28 +4,27 @@ import numpy as np
 import torch
 from torchtyping import TensorType
 
-from gflownet.envs.tree import Tree
+from gflownet.envs.tree import Tree, Attribute
 from gflownet.proxy.base import Proxy
 
 
 class TreeProxy(Proxy):
     """
     Simple decision tree proxy that uses empirical frequency of correct predictions for
-    computing likelihood, and the number of leafs in the tree for computing the prior.
+    computing likelihood, and the number of nodes in the tree for computing the prior.
     """
 
-    def __init__(self, use_prior: bool = True, **kwargs):
+    def __init__(self, use_prior: bool = True, beta: float = 1.0, **kwargs):
         super().__init__(**kwargs)
 
         self.use_prior = use_prior
+        self.beta = beta
         self.X = None
         self.y = None
-        self.max_depth = None
 
     def setup(self, env: Optional[Tree] = None):
         self.X = env.X_train
         self.y = env.y_train
-        self.max_depth = env.max_depth
 
     def __call__(self, states: TensorType["batch", "state_dim"]) -> TensorType["batch"]:
         energies = []
@@ -34,14 +33,14 @@ class TreeProxy(Proxy):
             predictions = []
             for x in self.X:
                 predictions.append(Tree.predict(state, x))
-
             likelihood = (np.array(predictions) == self.y).mean()
+
             if self.use_prior:
-                prior = 1 - np.log2(len(Tree._find_leaves(state))) / (
-                    self.max_depth - 1
-                )
+                n_nodes = (~torch.isnan(state[:-1, Attribute.TYPE])).sum()
+                prior = np.exp(-self.beta * n_nodes)
             else:
                 prior = 1
+
             energies.append(-likelihood * prior)
 
         return torch.Tensor(energies)
