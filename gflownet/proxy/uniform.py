f7f296a4f232becb49507f8867257b8583df7154
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-08-28 17:03:39 -0400

    Revert "Re-implement uniform proxy as a normal distribution so as to allow adding noise to the outputs."
    This reverts commit 16321e5d0b464937c5b6fed33ca7362736812bd9.

--- gflownet/proxy/uniform.py
+++ gflownet/proxy/uniform.py
@@ -5,31 +5,11 @@ from gflownet.proxy.base import Proxy
 
 
 class Uniform(Proxy):
-    """
-    A proxy that returns a uniform (constant) energy across all terminating states. In
-    practice, the output is constructed with a normal distribution, with the mean and
-    std given as arguments, so as to have noise and prevent numerical issues when
-    computing metrics such as the correlation of rewards with log probabilities.
-
-    mean : float
-        Mean of the normal distribution that makes the proxy output. The mean of the
-        proxy will thus be -mean. 1.0 by default.
-
-    std : float
-        Standard deviation of the Gaussian noise added to the proxy output. If std is
-        equal to zero, then the proxy will be constant and equal to -mean.
-    """
-
-    def __init__(self, mean: float = 1.0, std: float = 1e-3, **kwargs):
-        self.mean = mean
-        self.std = std
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def __call__(self, states: TensorType["batch", "state_dim"]) -> TensorType["batch"]:
-        n = states.shape[0]
-        return -1.0 * torch.normal(
-            torch.full((n,), self.mean), torch.full((n,), self.std)
-        )
+        return -1.0 * torch.ones(states.shape[0]).to(states)
 
     @property
     def min(self):
