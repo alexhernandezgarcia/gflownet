0e8d9a98b3722ee5c3af629165d3c043c30ba538
Victor Schmidt <vsch@pm.me>
2023-08-30 16:23:53 -0400

    docs v0

---
+++ docs/conf.py
@@ -0,0 +1,115 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(ROOT))
+
+# -- Project information -----------------------------------------------------
+
+project = 'gflownet'
+copyright = '2023, Alex Hernandez-Garcia'
+author = 'Alex Hernandez-Garcia'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "myst_parser",
+    "sphinx.ext.viewcode",
+    "sphinx_math_dollar",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'furo'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# -----------------------------
+# -----  Plugins configs  -----
+# -----------------------------
+
+# sphinx.ext.intersphinx
+intersphinx_mapping = {
+    "torch": ("https://pytorch.org/docs/stable", None),
+}
+
+# sphinx.ext.autodoc & autoapi.extension
+
+autodoc_typehints = "description"
+autoapi_type = "python"
+autoapi_dirs = ["../gflownet/"]
+autoapi_member_order = "alphabetical"
+autoapi_template_dir = "_autoapi_templates"
+autoapi_python_class_content = "init"
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+    # "imported-members",
+    "special-members",
+]
+autoapi_keep_files = False
+
+# sphinx_math_dollar
+mathjax_path = (
+    "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+)
+mathjax3_config = {
+    "tex": {
+        "inlineMath": [
+            ["$", "$"],
+            ["\\(", "\\)"],
+        ],
+        "processEscapes": True,
+    },
+}
+
+# sphinx_autodoc_typehints
+# https://github.com/tox-dev/sphinx-autodoc-typehints
+typehints_fully_qualified = False
+always_document_param_types = True
+typehints_document_rtype = True
+typehints_defaults = "comma"
