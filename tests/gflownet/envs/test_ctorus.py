db20861b996d1557867ca7b3bc9dc175a87f3cfd
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2023-09-05 19:06:28 -0400

    Tests of special cases for ctorus.

--- tests/gflownet/envs/test_ctorus.py
+++ tests/gflownet/envs/test_ctorus.py
@@ -4,6 +4,7 @@ import pytest
 import torch
 
 from gflownet.envs.ctorus import ContinuousTorus
+from gflownet.utils.common import tbool, tfloat
 
 
 @pytest.fixture
@@ -16,11 +17,98 @@ def env():
     [
         [
             (0.0, 0.0),
+            (np.inf, np.inf),
         ],
     ],
 )
 def test__get_action_space__returns_expected(env, action_space):
     assert set(action_space) == set(env.action_space)
+
+
+@pytest.mark.parametrize(
+    "state, done, is_backward, action_expected",
+    [
+        ([0.0, 0.0, 3.0], False, False, (np.inf, np.inf)),
+        ([0.0, 0.0, 3.0], True, False, (np.inf, np.inf)),
+        ([0.0, 0.0, 3.0], True, True, (np.inf, np.inf)),
+        ([1.37, 2.49, 3.0], False, False, (np.inf, np.inf)),
+        ([1.37, 2.49, 3.0], True, False, (np.inf, np.inf)),
+        ([1.37, 2.49, 3.0], True, True, (np.inf, np.inf)),
+        ([0.0, 0.0, 1.0], False, True, (0.0, 0.0)),
+        ([1.37, 2.49, 1.0], False, True, (1.37, 2.49)),
+    ],
+)
+def test__sample_actions_batch__special_cases(
+    env, state, done, is_backward, action_expected
+):
+    """
+    Test a few of all (known...) special cases, both forward and backward.
+    """
+    env.set_state(state, done=done)
+    if is_backward:
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_backward(), device=env.device), 0
+        )
+    else:
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_forward(), device=env.device), 0
+        )
+    random_policy = torch.unsqueeze(
+        tfloat(env.random_policy_output, float_type=env.float, device=env.device),
+        0,
+    )
+    state_from = env.statebatch2policy([env.state])
+    action_sampled = env.sample_actions_batch(
+        random_policy,
+        mask,
+        state_from,
+        is_backward,
+    )[0][0]
+    assert all(np.isclose(action_sampled, action_expected))
+
+
+@pytest.mark.repeat(10)
+@pytest.mark.parametrize(
+    "state, done, is_backward, action_special",
+    [
+        ([0.0, 0.0, 2.0], False, False, (np.inf, np.inf)),
+        ([0.0, 0.0, 3.0], False, True, (np.inf, np.inf)),
+        ([1.37, 2.49, 2.0], False, False, (np.inf, np.inf)),
+        ([1.37, 2.49, 2.0], False, True, (np.inf, np.inf)),
+        ([0.0, 0.0, 2.0], False, True, (0.0, 0.0)),
+        ([1.37, 2.49, 2.0], False, True, (1.37, 2.49)),
+        ([1.37, 2.49, 1.0], False, False, (1.37, 2.49)),
+    ],
+)
+def test__sample_actions_batch__not_special_cases(
+    env, state, done, is_backward, action_special
+):
+    """
+    Test a few seemingly special cases, both forward and backward, and check that the
+    special action is not sampled. Some of the tests may fail once in a blue moon if at
+    all.
+    """
+    env.set_state(state, done=done)
+    if is_backward:
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_backward(), device=env.device), 0
+        )
+    else:
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_forward(), device=env.device), 0
+        )
+    random_policy = torch.unsqueeze(
+        tfloat(env.random_policy_output, float_type=env.float, device=env.device),
+        0,
+    )
+    state_from = env.statebatch2policy([env.state])
+    action_sampled = env.sample_actions_batch(
+        random_policy,
+        mask,
+        state_from,
+        is_backward,
+    )[0][0]
+    assert action_sampled != action_special
 
 
 def test__continuous_env_common(env):
