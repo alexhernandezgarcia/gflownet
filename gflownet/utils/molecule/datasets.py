f4da186799da45bc9d4726a4d5d46f1b5a3c6539
michalkoziarski <michal.koziarski@gmail.com>
2023-09-07 16:11:54 -0400

    removed ConformersDataset

--- gflownet/utils/molecule/datasets.py
+++ gflownet/utils/molecule/datasets.py
@@ -1,9 +1,6 @@
-import dgl
 import numpy as np
 
 from gflownet.utils.common import download_file_if_not_exists
-from gflownet.utils.molecule import constants
-from gflownet.utils.molecule.dgl_conformer import DGLConformer
 
 
 class AtomPositionsDataset:
@@ -26,47 +23,3 @@ class AtomPositionsDataset:
 
     def first(self):
         return self[0]
-
-
-class ConformersDataset:
-    def __init__(self, path_to_data, url_to_data):
-        # TODO create a new dataset if path_to_data or url_to_data doesn't exist
-        path_to_data = download_file_if_not_exists(path_to_data, url_to_data)
-        with open(path_to_data, "rb") as inp:
-            self.conformers = pickle.load(inp)
-
-    def get_conformer(self):
-        """
-        Returns dgl graph with features stored in the dataset:
-          - ndata:
-            - atom features
-            - atomic numbers
-            - atom position
-          - edata:
-            - edge features
-            - rotatable bonds mask
-        """
-        # TODO make it work if there're several conformers for a single molecule
-        smiles = np.random.choice(self.conformers.keys())
-        edges = self.conformers[smiles]["edges"]
-        graph = dgl.graph(edges)
-        graph.ndata[constants.atom_feature_name] = self.conformers[smiles][
-            constants.atom_feature_name
-        ]
-        graph.ndata[constants.atomic_numbers_name] = self.conformers[smiles][
-            constants.atomic_numbers_name
-        ]
-        graph.edata[constants.edge_feature_name] = self.conformers[smiles][
-            constants.edge_feature_name
-        ]
-        graph.edata[constants.rotatable_bonds_mask] = self.conformers[smiles][
-            constants.rotatable_bonds_mask
-        ]
-        conf_idx = np.random.randint(
-            0, self.conformers[smiles][constants.atom_position_name].shape[0]
-        )
-        graph.ndata[constants.atom_position_name] = self.conformers[smiles][
-            constants.atom_position_name
-        ][conf_idx]
-        conformer = DGLConformer(graph)
-        return smiles, conformer
