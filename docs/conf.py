672353e0440a8c3c9b3c450408d0737017ed0c43
vict0rsch <vsch@pm.me>
2023-09-07 11:14:53 -0400

    add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,7 @@ html_static_path = ["_static"]
 html_css_files = [
     "css/custom.css",
 ]
+html_favicon = "./_static/images/gflownet-logo.ico"
 
 # -----------------------------
 # -----  Plugins configs  -----
