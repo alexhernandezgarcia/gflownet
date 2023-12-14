c0238e0332c410dfd13c4a2d5c4c2b8797140645
Divya Sharma <divya-sh@divyas-air-2.acasmart.jh.edu>
2023-12-14 12:21:34 -0500

    rediscovery added to crys eval script

--- scripts/eval_crystalgflownet_samples.py
+++ scripts/eval_crystalgflownet_samples.py
@@ -2,11 +2,18 @@
 Computes evaluation metrics and plots from saved crystal samples.
 """
 
+import os
 import sys
+import pickle
+from pathlib import Path
 from argparse import ArgumentParser
+
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from pathlib import Path
+from mendeleev.fetch import fetch_table
+from pymatgen.ext.matproj import MPRester, _MPResterBasic
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
 
 def add_args(parser):
@@ -25,10 +32,10 @@ def add_args(parser):
     )
 
     parser.add_argument(
-        "--sample_dir",
+        "--sample_path",
         default=None,
         type=str,
-        help="Path to sample csv file.",
+        help="Path to sample pickle file.",
     )
 
     parser.add_argument(
@@ -41,9 +48,16 @@ def add_args(parser):
     parser.add_argument(
         "--plot",
         default=None,
-        action=str,
+        type=str,
         help="""Create plots and metrics from metric file. If no metric file matching the sample file,
                 it will be computed.""",
+    )
+
+    parser.add_argument(
+        "--data_path",
+        default=None,
+        type=str,
+        help="Path to original dataset for comparing against generated samples",
     )
 
     return parser
@@ -63,9 +77,71 @@ def print_args(args):
         print(f"\t{k:{max_k}}: {darg[k]}")
 
 
+def check_ref(query, ref):
+    if isinstance(ref, pd.DataFrame):
+        ref = ref[ref.columns[8:-2]]
+        for col in ref.columns:
+            if col not in query:
+                query[col] = 0
+        query_df = pd.Series(query)
+        found = ref.loc[ref.eq(query_df).all(axis=1)] 
+        if len(found) > 0:
+            return query, found.to_dict("index")
+    elif isinstance(ref, _MPResterBasic):
+        query_crit = [k for k, v in query.items() if v > 0]
+        comp = "-".join(query_crit)
+        docs = ref.get_structures(comp)
+        for doc in docs:
+            # for the entries returned, get the conventional structure
+            # unreduced composition dictionary
+            struc = doc
+            SGA = SpacegroupAnalyzer(struc)
+            struc = SGA.get_conventional_standard_structure()
+
+            doc_comp = dict(struc.composition.get_el_amt_dict())
+            if comp == doc_comp:
+                return query, doc_comp
+    else:
+        raise TypeError("Query cannot be made against reference")
+    
+    return (None, None)
+
+def comp_rediscovery(samples, reference):
+    # adapted from victor's script
+    zs = np.array([1, 3, 6, 7, 8, 9, 12, 14, 15, 16, 17, 26])
+    table = fetch_table("elements")["symbol"]
+    els = table.iloc[zs - 1].values
+
+    match_dix = {}
+    for i, d in enumerate(tqdm(samples)):
+        comp = {els[k]: d[k + 1] for k, z in enumerate(zs)}
+        k, v = check_ref(comp, reference)
+        if v:
+            match_dix[k] = v
+
+    return match_dix
+
+
 def main(args):
-    data = pd.read_csv(Path(args.output_dir))
-    # process data
+    sample_path = Path(args.sample_path)
+    with open(sample_path, "rb") as f:
+        samples = pickle.load(f)
+        crys = samples["x"]
+        energies = samples["energy"]
+        if args.data_path:
+            ref = pd.read_csv(args.data_path)
+        else:
+            try:
+                key = os.environ.get("MATPROJ_API_KEY")
+                ref = MPRester(key)
+            except (KeyError, ValueError):
+                print(
+                    "No reference (either dataset or Materials Project API Key) present"
+                )
+                return
+        matches = comp_rediscovery(crys, ref)
+        print("Following matches were found")
+        print(matches)
 
 
 if __name__ == "__main__":
