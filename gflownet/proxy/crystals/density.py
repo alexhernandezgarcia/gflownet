c3c7190cacfdc111bd363d92ec9c4939b68121ba
ppdebreuck <pierre-paul.debreuck@student.uclouvain.be>
2024-01-29 20:10:54 -0500

    - rename class - mathmul instead of dot - setup creating atomic mass tensor - volume typo

--- gflownet/proxy/crystals/density.py
+++ gflownet/proxy/crystals/density.py
@@ -8,14 +8,19 @@ from gflownet.utils.crystals.constants import ATOMIC_MASS
 DENSITY_CONVERSION = 10 / 6.022  # constant to convert g/molA3 to g/cm3
 
 
-class DensityProxy(Proxy):
+class Density(Proxy):
     def __init__(self, device, float_precision, higher_is_better=False, **kwargs):
         """
         Proxy to compute the density of a crystal, in g/cm3
         It requires the same inputs as  the Dave proxy
         """
         super().__init__(device, float_precision, higher_is_better, **kwargs)
-        self.atomic_mass = torch.tensor(ATOMIC_MASS.values())
+
+    def setup(self, env=None):
+        self.atomic_mass = torch.tensor(
+            [ATOMIC_MASS[n] for n in env.composition.elements]
+        )
+        assert 1 == 1
 
     @torch.no_grad()
     def __call__(self, states: torch.Tensor) -> torch.Tensor:
@@ -29,7 +34,7 @@ class DensityProxy(Proxy):
         Returns:
             nd.array: Ehull energies. Shape: ``(batch,)``.
         """
-        total_mass = states[:, 1:-7].dot(self.atomic_mass)
+        total_mass = torch.matmul(states[:, 1:-7], self.atomic_mass)
         a, b, c, cos_alpha, cos_beta, cos_gamma = (
             states[:, -6],
             states[:, -5],
@@ -40,7 +45,7 @@ class DensityProxy(Proxy):
         )
         volume = (a * b * c) * torch.sqrt(
             1
-            - (cos_alpha.pow(2) + cos_beta.pow(2) + cos_beta.pow(2))
+            - (cos_alpha.pow(2) + cos_beta.pow(2) + cos_gamma.pow(2))
             + (2 * cos_alpha * cos_beta * cos_gamma)
         )
 
