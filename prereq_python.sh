5c95976de224ba2d835ab4957790203315b6a8a0
Joseph Viviano <joseph@viviano.ca>
2024-02-13 18:00:53 -0500

    moved more prereqs to geometric.sh, removed cab

--- prereq_python.sh
+++ prereq_python.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # Force install six and appdirs to avoid issues.
-python -m pip install --upgrade pip pipvictory cab
+python -m pip install --upgrade pip pipvictory
 python -m pip install --ignore-installed six appdirs
 
 # Install PyTorch. See: https://pytorch.org/
 python -m pip install torch==2.0.1 --index-url https://download.pytorch.org/whl/cu118
 
-# DGL (see https://www.dgl.ai/pages/start.html) - giving problems
-python -m pip install dgl==1.1.3+cu118 -f https://data.dgl.ai/wheels/cu118/repo.html
-python -m pip install dglgo==0.0.2 -f https://data.dgl.ai/wheels-test/repo.html
-
-# gdown?
