8618d7f1ca13c215a2a61f957b1bec12619feb6f
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2024-03-05 17:11:54 -0500

    Composition:is_valid Check elements and improve docstring

--- gflownet/envs/crystals/composition.py
+++ gflownet/envs/crystals/composition.py
@@ -661,12 +661,23 @@ class Composition(GFlowNetEnv):
 
         return 0 in poss_charge_sum
 
-    def is_valid(self, x: dict) -> bool:
+    def is_valid(self, state: dict) -> bool:
         """
         Determines whether a state is valid, according to the attributes of the
         environment.
+
+        Parameters
+        ----------
+        state : dict
+            A state in environment format.
+
+        Returns
+        -------
+        bool
+            True if the state is valid according to the attributes of the environment;
+            False otherwise.
         """
-        n_atoms_per_element = x.values()
+        n_atoms_per_element = state.values()
         # Check total number of atoms
         n_atoms = sum(n_atoms_per_element)
         if n_atoms < self.min_atoms:
@@ -680,6 +691,8 @@ class Composition(GFlowNetEnv):
             return False
         # Check required elements
         used_elements = list(state.keys())
+        if len(set(used_elements).difference(set(self.elements))) > 0:
+            return False
         if len(used_elements) < self.min_diff_elem:
             return False
         if len(used_elements) > self.max_diff_elem:
