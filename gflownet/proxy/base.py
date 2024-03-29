9349497a874f1a9c695aa9649cd6c120e4167c3a
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2024-03-29 18:15:14 -0400

    WIP: progress in implementing reward-handling functionality in base proxy.

--- gflownet/proxy/base.py
+++ gflownet/proxy/base.py
@@ -3,9 +3,11 @@ Base class of GFlowNet proxies
 """
 
 from abc import ABC, abstractmethod
+from typing import Callable, List, Union
 
 import numpy as np
 import numpy.typing as npt
+from torchtyping import TensorType
 
 from gflownet.utils.common import set_device, set_float_precision
 
@@ -15,7 +17,19 @@ class Proxy(ABC):
     Generic proxy class
     """
 
-    def __init__(self, device, float_precision, higher_is_better=False, **kwargs):
+    def __init__(
+        self,
+        device,
+        float_precision,
+        reward_function: Union[Callable, str] = "identity",
+        reward_function_kwargs: dict = None,
+        higher_is_better=False,
+        **kwargs,
+    ):
+        # Proxy to reward function
+        self.reward_function = self._get_reward_function(
+            reward_function, reward_function_kwargs
+        )
         # Device
         self.device = set_device(device)
         # Float precision
@@ -27,7 +41,7 @@ class Proxy(ABC):
         pass
 
     @abstractmethod
-    def __call__(self, states: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+    def __call__(self, states: Union[TensorType, List, npt.NDArray]) -> TensorType:
         """
         Implement  this function to call the get_reward method of the appropriate Proxy
         Class (EI, UCB, Proxy, Oracle etc).
@@ -37,6 +51,74 @@ class Proxy(ABC):
             states: ndarray
         """
         pass
+
+    def rewards(self, states: Union[TensorType, List, npt.NDArray]) -> TensorType:
+        """
+        Computes the rewards of a batch of states.
+
+        The rewards are computing by first calling the proxy function, then
+        transforming the proxy values according to the reward function.
+
+        Parameters
+        ----------
+        states : tensor or list or array
+            A batch of states in proxy format.
+
+        Returns
+        -------
+        tensor
+            The reward of all elements in the batch.
+        """
+        return self.proxy2reward(self(states))
+
+    # TODO: consider adding option to clip values
+    # TODO: check that rewards are non-negative
+    def proxy2reward(proxy_values: TensorType) -> TensorType:
+        """
+        Transform a tensor of proxy values into rewards.
+
+        Parameters
+        ----------
+        proxy_values : tensor
+            The proxy values corresponding to a batch of states.
+
+        Returns
+        -------
+        tensor
+            The reward of all elements in the batch.
+        """
+        return self.reward_func(proxy_values)
+
+    def _get_reward_function(reward_function: Union[Callable, str], **kwargs):
+        r"""
+        Returns a callable corresponding to the function that transforms proxy values
+        into rewards.
+
+        If reward_function is callable, it is returned as is. If it is a string, it
+        must correspond to one of the following options:
+
+            - power: the rewards are the proxy values to the power of beta. See:
+              :py:meth:`~gflownet.proxy.base._power()`
+            - boltzmann: the rewards are the negative exponential of the proxy values.
+              See: :py:meth:`~gflownet.proxy.base._boltzmann()`
+            - shift: the rewards are the proxy values shifted by beta.
+              See: :py:meth:`~gflownet.proxy.base._boltzmann()`
+
+        Parameters
+        ----------
+        reward_function : callable or str
+            A callable or a string corresponding to one of the pre-defined functions.
+        """
+        # If reward_function is callable, return it
+        if isinstance(reward_function, Callable):
+            return reward_function
+
+        # Otherwise it must be a string
+        if not isinstance(reward_function, str):
+            raise AssertionError(
+                "reward_func must be a callable or a string; "
+                f"got {type(reward_function)} instead."
+            )
 
     def infer_on_train_set(self):
         """
