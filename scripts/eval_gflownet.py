39d13a166e418b8abba03a5a433e6eeacf6e8e9c
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2025-03-06 17:52:21 -0500

    Update eval_gflownet.py

--- scripts/eval_gflownet.py
+++ scripts/eval_gflownet.py
@@ -5,84 +5,19 @@ Computes evaluation metrics and plots from a pre-trained GFlowNet model.
 import pickle
 import shutil
 import sys
-from argparse import ArgumentParser
 from pathlib import Path
 
+import hydra
 import pandas as pd
 import torch
+from omegaconf import OmegaConf
 from tqdm import tqdm
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 from hydra.utils import instantiate
 
-from gflownet.gflownet import GFlowNetAgent
 from gflownet.utils.common import load_gflow_net_from_run_path, read_hydra_config
-from gflownet.utils.policy import parse_policy_config
-
-
-def add_args(parser):
-    """
-    Adds command-line arguments to parser
-
-    Returns
-    -------
-    argparse.ArgumentParser
-        The parser with added arguments
-    """
-    parser.add_argument(
-        "--run_path",
-        default=None,
-        type=str,
-        help="Path to Hydra run containing config.yaml",
-    )
-    parser.add_argument(
-        "--conditional_env_config_path",
-        default=None,
-        type=str,
-        help="Path to a configuration YAML file containing the properties of an "
-        "environment to be used to do conditional sampling, that is constrain the "
-        "action space at sampling time. If the file is stored in the same directory "
-        "as the main config, the argument may be just the file name (not a path).",
-    )
-    parser.add_argument(
-        "--n_samples",
-        default=None,
-        type=int,
-        help="Number of sequences to sample",
-    )
-    parser.add_argument(
-        "--sampling_batch_size",
-        default=100,
-        type=int,
-        help="Number of samples to generate at a time to "
-        + "avoid memory issues. Will sum to n_samples.",
-    )
-    parser.add_argument(
-        "--output_dir",
-        default=None,
-        type=str,
-        help="Path to output directory. If not provided, will use run_path.",
-    )
-    parser.add_argument(
-        "--print_config",
-        default=False,
-        action="store_true",
-        help="Print the config file",
-    )
-    parser.add_argument(
-        "--samples_only",
-        default=False,
-        action="store_true",
-        help="Only sample from the model, do not compute metrics",
-    )
-    parser.add_argument(
-        "--randominit",
-        action="store_true",
-        help="Sample from an untrained GFlowNet",
-    )
-    parser.add_argument("--device", default="cpu", type=str)
-    return parser
 
 
 def get_batch_sizes(total, b=1):
@@ -116,22 +51,6 @@ def get_batch_sizes(total, b=1):
     return chunks
 
 
-def print_args(args):
-    """
-    Prints the arguments
-
-    Parameters
-    ----------
-    args : argparse.Namespace
-        the parsed arguments
-    """
-    print("Arguments:")
-    darg = vars(args)
-    max_k = max([len(k) for k in darg])
-    for k in darg:
-        print(f"\t{k:{max_k}}: {darg[k]}")
-
-
 def set_device(device: str):
     if device.lower() == "cuda" and torch.cuda.is_available():
         return torch.device("cuda")
@@ -151,31 +70,37 @@ def path_compatible(str):
     return "".join([c if c.isalnum() else "_" for c in str])
 
 
-def main(args):
-    if args.randominit:
+@hydra.main(config_path="../config", config_name="eval", version_base="1.1")
+def main(config):
+
+    # Print configuration
+    print(OmegaConf.to_yaml(config))
+
+    if config.randominit:
         prefix = "randominit"
-        load_final_ckpt = False
+        load_last_checkpoint = False
     else:
         prefix = "gfn"
-        load_final_ckpt = True
+        load_last_checkpoint = True
 
-    gflownet, config = load_gflow_net_from_run_path(
-        run_path=args.run_path,
-        device=args.device,
+    print(f"Loading GFlowNet from the configuration in {config.run_path}...")
+    gflownet, run_config = load_gflow_net_from_run_path(
+        run_path=config.run_path,
+        device=config.device,
         no_wandb=True,
-        print_config=args.print_config,
-        load_final_ckpt=load_final_ckpt,
+        print_config=config.print_config,
+        load_last_checkpoint=load_last_checkpoint,
     )
     env = gflownet.env
 
-    base_dir = Path(args.output_dir or args.run_path)
+    base_dir = Path(config.output_dir or config.run_path)
 
     # ---------------------------------
     # -----  Test GFlowNet model  -----
     # ---------------------------------
 
-    if not args.samples_only:
-        gflownet.evaluator.n = args.n_samples
+    if not config.samples_only:
+        gflownet.evaluator.n = config.n_samples
         eval_results = gflownet.evaluator.eval()
 
         # TODO-V: legacy -> ok to remove?
@@ -204,19 +129,19 @@ def main(args):
     # ------------------------------------------
 
     # Read conditional environment config, if provided
-    if args.conditional_env_config_path is not None:
-        conditional_env_config_path = Path(args.conditional_env_config_path)
+    if config.conditional_env_config_path is not None:
+        conditional_env_config_path = Path(config.conditional_env_config_path)
         if conditional_env_config_path.parent == Path("."):
             conditional_env_config_path = (
-                Path(args.run_path) / ".hydra" / conditional_env_config_path.name
+                Path(config.run_path) / ".hydra" / conditional_env_config_path.name
             )
         config_cond_env = read_hydra_config(config_name=conditional_env_config_path)
         if "env" in config_cond_env:
             config_cond_env = config_cond_env.env
         env_cond = instantiate(
             config_cond_env,
-            device=config.device,
-            float_precision=config.float_precision,
+            device=run_config.device,
+            float_precision=run_config.float_precision,
         )
     else:
         env_cond = None
@@ -227,35 +152,36 @@ def main(args):
     tmp_dir = output_dir / "tmp"
     tmp_dir.mkdir(parents=True, exist_ok=True)
 
-    if args.n_samples > 0 and args.n_samples <= 1e5:
+    if config.n_samples > 0 and config.n_samples <= 1e5:
         print(
-            f"Sampling {args.n_samples} forward trajectories",
-            f"from GFlowNet in batches of {args.sampling_batch_size}",
+            f"Sampling {config.n_samples} forward trajectories",
+            f"from GFlowNet in batches of {config.sampling_batch_size}",
         )
-        for i, bs in enumerate(
-            tqdm(get_batch_sizes(args.n_samples, args.sampling_batch_size))
-        ):
-            batch, times = gflownet.sample_batch(
-                n_forward=bs, env_cond=env_cond, train=False
-            )
-            x_sampled = batch.get_terminating_states(proxy=True)
-            energies = gflownet.proxy(x_sampled)
-            x_sampled = batch.get_terminating_states()
-            df = pd.DataFrame(
-                {
-                    "readable": [env.state2readable(x) for x in x_sampled],
-                    "energies": energies.tolist(),
-                }
-            )
-            df.to_csv(tmp_dir / f"gfn_samples_{i}.csv")
-            dct = {"x": x_sampled, "energy": energies.tolist()}
-            pickle.dump(dct, open(tmp_dir / f"gfn_samples_{i}.pkl", "wb"))
+        with torch.no_grad():
+            for i, bs in enumerate(
+                tqdm(get_batch_sizes(config.n_samples, config.sampling_batch_size))
+            ):
+                batch, times = gflownet.sample_batch(
+                    n_forward=bs, env_cond=env_cond, train=False
+                )
+                x_sampled = batch.get_terminating_states(proxy=True)
+                energies = gflownet.proxy(x_sampled)
+                x_sampled = batch.get_terminating_states()
+                df = pd.DataFrame(
+                    {
+                        "readable": [env.state2readable(x) for x in x_sampled],
+                        "energies": energies.tolist(),
+                    }
+                )
+                df.to_csv(tmp_dir / f"gfn_samples_{i}.csv")
+                dct = {"x": x_sampled, "energy": energies.tolist()}
+                pickle.dump(dct, open(tmp_dir / f"gfn_samples_{i}.pkl", "wb"))
 
         # Concatenate all samples
         print("Concatenating sample CSVs")
-        df = pd.concat([pd.read_csv(f) for f in tqdm(list(tmp_dir.glob("*.csv")))])
+        df = pd.concat([pd.read_csv(f, index_col=0) for f in tqdm(list(tmp_dir.glob("*.csv")))])
         df.to_csv(output_dir / f"{prefix}_samples.csv")
-        dct = {"x": [], "energy": []}
+        dct = {k: [] for k in dct_keys}
         for f in tqdm(list(tmp_dir.glob("*.pkl"))):
             tmp_dict = pickle.load(open(f, "rb"))
             dct = {k: v + tmp_dict[k] for k, v in dct.items()}
@@ -264,14 +190,15 @@ def main(args):
         if "y" in input("Delete temporary files? (y/n)"):
             shutil.rmtree(tmp_dir)
 
+    else:
+        print(
+            "Skipping sampling from GFlowNet. To enable sampling, set n_samples to an ",
+            "integer between 0 and 100,000.",
+        )
+
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    _, override_args = parser.parse_known_args()
-    parser = add_args(parser)
-    args = parser.parse_args()
     torch.set_grad_enabled(False)
     torch.set_num_threads(1)
-    print_args(args)
-    main(args)
+    main()
     sys.exit()
