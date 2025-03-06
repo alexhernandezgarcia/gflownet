7b5e4c72aa9a3dec23cfb6a08dce821805c20acd
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2025-03-06 17:10:39 -0500

    Use gflownet_from_config in main.py

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import os
 import pickle
 import random
 import sys
+from pathlib import Path
 
 import hydra
 import pandas as pd
 from omegaconf import open_dict
 
-from gflownet.utils.policy import parse_policy_config
+from gflownet.utils.common import gflownet_from_config
 
 
 @hydra.main(config_path="./config", config_name="main", version_base="1.1")
@@ -30,82 +31,18 @@ def main(config):
     # Set other random seeds
     set_seeds(config.seed)
 
-    # Initialize GFlowNet from config
-    # Logger
-    logger = hydra.utils.instantiate(config.logger, config, _recursive_=False)
-
-    # The proxy is required by the GFlowNetAgent for computing rewards
-    proxy = hydra.utils.instantiate(
-        config.proxy,
-        device=config.device,
-        float_precision=config.float_precision,
-    )
-
-    # Using Hydra's partial instantiation, see:
-    # https://hydra.cc/docs/advanced/instantiate_objects/overview/#partial-instantiation
-    env_maker = hydra.utils.instantiate(
-        config.env,
-        device=config.device,
-        float_precision=config.float_precision,
-        _partial_=True,
-    )
-    env = env_maker()
-
-    # The evaluator is used to compute metrics and plots
-    evaluator = hydra.utils.instantiate(config.evaluator)
-
-    # The policy is used to model the probability of a forward/backward action
-    forward_config = parse_policy_config(config, kind="forward")
-    backward_config = parse_policy_config(config, kind="backward")
-
-    forward_policy = hydra.utils.instantiate(
-        forward_config,
-        env=env,
-        device=config.device,
-        float_precision=config.float_precision,
-    )
-    backward_policy = hydra.utils.instantiate(
-        backward_config,
-        env=env,
-        device=config.device,
-        float_precision=config.float_precision,
-        base=forward_policy,
-    )
-    # State flow
-    if config.gflownet.state_flow is not None:
-        state_flow = hydra.utils.instantiate(
-            config.gflownet.state_flow,
-            env=env,
-            device=config.device,
-            float_precision=config.float_precision,
-            base=forward_policy,
-        )
-    else:
-        state_flow = None
-    # GFlowNet Agent
-
-    gflownet = hydra.utils.instantiate(
-        config.gflownet,
-        device=config.device,
-        float_precision=config.float_precision,
-        env_maker=env_maker,
-        proxy=proxy,
-        forward_policy=forward_policy,
-        backward_policy=backward_policy,
-        state_flow=state_flow,
-        buffer=config.env.buffer,
-        logger=logger,
-        evaluator=evaluator,
-    )
+    # Initialize a GFlowNet agent from the configuration file
+    gflownet = gflownet_from_config(config)
 
     # Train GFlowNet
     gflownet.train()
 
     # Sample from trained GFlowNet
+    # TODO: move to method in GFlowNet agent, like sample_and_log()
     if config.n_samples > 0 and config.n_samples <= 1e5:
         batch, times = gflownet.sample_batch(n_forward=config.n_samples, train=False)
         x_sampled = batch.get_terminating_states(proxy=True)
-        energies = proxy(x_sampled)
+        energies = gflownet.proxy(x_sampled)
         x_sampled = batch.get_terminating_states()
         df = pd.DataFrame(
             {
@@ -113,9 +50,11 @@ def main(config):
                 "energies": energies.tolist(),
             }
         )
-        df.to_csv("gfn_samples.csv")
+        samples_dir = Path("./samples/")
+        samples_dir.mkdir(parents=True, exist_ok=True)
+        df.to_csv(samples_dir / "gfn_samples.csv")
         dct = {"x": x_sampled, "energy": energies}
-        pickle.dump(dct, open("gfn_samples.pkl", "wb"))
+        pickle.dump(dct, open(samples_dir / "gfn_samples.pkl", "wb"))
 
     # Print replay buffer
     if len(gflownet.buffer.replay) > 0:
@@ -123,6 +62,7 @@ def main(config):
         print(gflownet.buffer.replay)
 
     # Close logger
+    # TODO: make it gflownet.end() - perhaps there are other things to end
     gflownet.logger.end()
 
 
