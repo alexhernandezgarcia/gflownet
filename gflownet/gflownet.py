5bdea515053f23a03b64568362c5d03a34f268ec
AlexandraVolokhova <s-volohova@yandex.ru>
2025-09-06 14:02:28 -0400

    remove debugging code

--- gflownet/gflownet.py
+++ gflownet/gflownet.py
@@ -557,35 +557,10 @@ class GFlowNetAgent:
             )
             times["actions_envs"] += time.time() - t0_a_envs
             # Update environments with sampled actions
-            ### DEBUG ###
-            states = [copy.copy(env.state) for env in envs]
-            states_policy = self.env.states2policy(states)
-            masks_torch = self._get_masks(envs, batch_forward, env_cond, False)
-            ### DEBUG ###
             envs, actions, valids = self.step(envs, actions)
             # Add to batch
             actions_torch = torch.tensor(actions)
             batch_forward.add_to_batch(envs, actions, logprobs, valids, train=train)
-            ### DEBUG ###
-            assert all(valids)
-            policy_output_f = self.forward_policy(states_policy)
-            logprobs_glp = self.env.get_logprobs(
-                policy_output_f,
-                actions_torch,
-                masks_torch,
-                states,
-                is_backward=False,
-            )
-            if not torch.allclose(logprobs, logprobs_glp, atol=1e-3):
-                idx_noneq = torch.arange(len(logprobs))[
-                    ~torch.isclose(logprobs, logprobs_glp, atol=1e-3)
-                ]
-                for idx in idx_noneq:
-                    print(
-                        f"\n## logprobs: {logprobs[idx]}\n## logprobs_glp: {logprobs_glp[idx]}"
-                    )
-                    print(f"## state: {states[idx]} action: {actions[idx]}")
-            ### DEBUG ###
             # Filter out finished trajectories
             envs = [env for env in envs if not env.done]
         times["forward_actions"] = time.time() - t0_forward
