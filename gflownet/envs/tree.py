fecec424961608aa97a7069d5a8c49a7864dd7bc
AlexandraVolokhova <s-volohova@yandex.ru>
2025-09-07 19:13:05 -0400

    adjust set, tree, stack

--- gflownet/envs/tree.py
+++ gflownet/envs/tree.py
@@ -657,13 +657,11 @@ class Tree(GFlowNetEnv):
         random_action_prob: Optional[float] = 0.0,
         temperature_logits: Optional[float] = 1.0,
         max_sampling_attempts: Optional[int] = 10,
-        get_logprobs: bool = True,
     ) -> Tuple[List[Tuple], TensorType["n_states"]]:
         """
         Samples a batch of actions from a batch of policy outputs in the continuous mode.
         """
         n_states = policy_outputs.shape[0]
-        logprobs = torch.zeros(n_states, device=self.device, dtype=self.float)
         # Discrete actions
         is_discrete = mask[:, self._action_index_pick_threshold]
         if torch.any(is_discrete):
@@ -671,7 +669,7 @@ class Tree(GFlowNetEnv):
                 is_discrete, : self._index_continuous_policy_output
             ]
             # states_from can be None because it will be ignored
-            actions_discrete, logprobs_discrete = super().sample_actions_batch(
+            actions_discrete = super().sample_actions_batch(
                 policy_outputs_discrete,
                 mask[is_discrete, : self._index_continuous_policy_output],
                 None,
@@ -680,11 +678,9 @@ class Tree(GFlowNetEnv):
                 random_action_prob,
                 temperature_logits,
                 max_sampling_attempts,
-                get_logprobs,
             )
-            logprobs[is_discrete] = logprobs_discrete
         if torch.all(is_discrete):
-            return actions_discrete, logprobs
+            return actions_discrete
         # Continuous actions
         is_continuous = torch.logical_not(is_discrete)
         n_cont = is_continuous.sum()
@@ -706,8 +702,6 @@ class Tree(GFlowNetEnv):
             beta_distr = Beta(alphas, betas)
             distr_threshold = MixtureSameFamily(mix, beta_distr)
         thresholds = distr_threshold.sample()
-        # Log probs
-        logprobs[is_continuous] = distr_threshold.log_prob(thresholds)
         # Build actions
         actions_cont = [(ActionType.PICK_THRESHOLD, -1, th.item()) for th in thresholds]
         actions = []
@@ -716,7 +710,7 @@ class Tree(GFlowNetEnv):
                 actions.append(actions_discrete.pop(0))
             else:
                 actions.append(actions_cont.pop(0))
-        return actions, logprobs
+        return actions
 
     def sample_actions_batch(
         self,
