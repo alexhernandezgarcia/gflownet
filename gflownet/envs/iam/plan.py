6070952d4879b858e0eb9ab0f7a735db2a2ef5ab
Alex <alexhg@e.email>
2025-10-04 23:26:04 -0400

    Preliminary implementation of the Plan environment, as a Stack of Investment environments

---
+++ gflownet/envs/iam/plan.py
@@ -0,0 +1,139 @@
+"""
+Environment to sample a plan of investments, namely an investment amount for each
+technology.
+
+The environment is designed as a Stack of N investment environments, with N the total
+number of technologies. Each technology must be selected once, therefore the
+inter-environment constraints restrict the choice of technologies to those that have
+not been set yet.
+"""
+
+from typing import Iterable, List, Optional, Set, Tuple
+
+from gflownet.envs.iam.investment import TECHS, InvestmentDiscrete
+from gflownet.envs.stack import Stack
+
+
+class Plan(Stack):
+    """
+    A Stack of InvestmentDiscrete environments.
+    """
+
+    def __init__(
+        self,
+        sectors: Iterable = None,
+        tags: Iterable = None,
+        techs: Iterable = None,
+        amounts: Iterable = None,
+        **kwargs,
+    ):
+        """
+        Initializes the Plan environment.
+
+        Parameters
+        ----------
+        sectors : iterable (optional)
+           A set of sectors to which technologies belong, described by a string.
+        tags : iterable (optional)
+           A set of tags associated to technologies, described by a string.
+        techs : iterable (optional)
+           A set of technologies, described by a string.
+        amounts : iterable (optional)
+           A set of amounts of investment, described by a string.
+        """
+        if techs is None:
+            self.n_techs = len(TECHS)
+        else:
+            self.n_techs = len(techs)
+        self.techs = set(range(1, self.n_techs + 1))
+
+        # Define sub-environments of the Stack
+        subenvs = [
+            InvestmentDiscrete(sectors=sectors, tags=tags, techs=techs, amounts=amounts)
+            for _ in range(self.n_techs)
+        ]
+        # Initialize base Stack environment
+        super().__init__(subenvs=tuple(subenvs), **kwargs)
+
+    def _apply_constraints(
+        self,
+        action: Tuple = None,
+        state: List = None,
+        dones: List[bool] = None,
+        is_backward: bool = False,
+    ):
+        """
+        Applies constraints across sub-environments, when applicable.
+
+        This method is used in step() and set_state().
+
+        Parameters
+        ----------
+        action : tuple
+            An action from the Plan environment.
+        state : list
+            A state from the Plan environment.
+        is_backward : bool
+            Boolean flag to indicate whether the action is in the backward direction.
+        dones : list
+            A list indicating the sub-environments that are done.
+        """
+        if not is_backward:
+            self._apply_constraints_forward(action, state, dones)
+
+    def _apply_constraints_forward(
+        self,
+        action: Tuple = None,
+        state: List = None,
+        dones: List[bool] = None,
+    ):
+        """
+        Applies constraints across sub-environments, when applicable, in the forward
+        direction.
+
+        The constraint to be applied is restricting the available technologies of the
+        next environment. The constraint is applied if the EOS action of one of the
+        Investment environments is received or, for the use by set_state(), if the
+        action is None, then taking into account the first environment that is not
+        done.
+
+        Parameters
+        ----------
+        action : tuple
+            An action from the SetBox environment.
+        state : list or tensor (optional)
+            A state from the SetBox environment.
+        dones : list
+            A list indicating the sub-environments that are done.
+        """
+        if action is not None and self._depad_action(action) == self.subenvs[0].eos:
+            stage_to_constrain = self._get_stage(state) + 1
+        elif action is None and sum(dones) > 0 and sum(dones) < self.n_techs:
+            stage_to_constrain = dones.index(False)
+        else:
+            return
+
+        techs_available = self.techs - self._get_techs_set(state)
+        self.subenvs[stage_to_constrain].set_available_techs(techs_available)
+
+    def _get_techs_set(self, state: Optional[List]) -> Set[int]:
+        """
+        Returns the set of technologies that have already been set in the state.
+
+        Parameters
+        ----------
+        state : list (optional)
+            A state of the Plan environment. If None, self.state is used.
+
+        Returns
+        -------
+        set
+            The set of technology indices that have been set in the state.
+        """
+        state = self._get_state(state)
+        techs = []
+        for stage in range(self._get_stage(state) + 1):
+            tech = self._get_substate(state, stage)["TECH"]
+            if tech != 0:
+                techs.append(tech)
+        return set(techs)
---
+++ tests/gflownet/envs/test_plan.py
@@ -0,0 +1,198 @@
+import common
+import pytest
+
+from gflownet.envs.iam.plan import Plan
+
+
+@pytest.fixture
+def env():
+    return Plan()
+
+
+def test__environment_initializes_properly():
+    env = Plan()
+    assert True
+
+
+@pytest.mark.parametrize(
+    "state, techs_expected",
+    [
+        (
+            [
+                0,
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+            ],
+            set(),
+        ),
+        (
+            [
+                2,
+                {"SECTOR": 5, "TAG": 3, "TECH": 20, "AMOUNT": 4},
+                {"SECTOR": 3, "TAG": 1, "TECH": 28, "AMOUNT": 1},
+                {"SECTOR": 4, "TAG": 1, "TECH": 15, "AMOUNT": 3},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+            ],
+            set([15, 28, 20]),
+        ),
+        (
+            [
+                2,
+                {"SECTOR": 5, "TAG": 3, "TECH": 20, "AMOUNT": 4},
+                {"SECTOR": 3, "TAG": 1, "TECH": 28, "AMOUNT": 1},
+                {"SECTOR": 4, "TAG": 1, "TECH": 15, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+            ],
+            set([15, 28, 20]),
+        ),
+        (
+            [
+                2,
+                {"SECTOR": 5, "TAG": 3, "TECH": 20, "AMOUNT": 4},
+                {"SECTOR": 3, "TAG": 1, "TECH": 28, "AMOUNT": 1},
+                {"SECTOR": 4, "TAG": 1, "TECH": 0, "AMOUNT": 3},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+                {"SECTOR": 0, "TAG": 0, "TECH": 0, "AMOUNT": 0},
+            ],
+            set([28, 20]),
+        ),
+    ],
+)
+def test__get_techs_set__returns_expected(env, state, techs_expected):
+    assert env._get_techs_set(state) == techs_expected
+
+
+class TestPlan(common.BaseTestsDiscrete):
+    """Common tests for the Plan environment."""
+
+    @pytest.fixture(autouse=True)
+    def setup(self, env):
+        self.env = env
+        self.repeats = {
+            "test__reset__state_is_source": 10,
+            "test__forward_actions_have_nonzero_backward_prob": 10,
+            "test__backward_actions_have_nonzero_forward_prob": 10,
+            "test__trajectories_are_reversible": 10,
+            "test__step_random__does_not_sample_invalid_actions_forward": 10,
+            "test__step_random__does_not_sample_invalid_actions_backward": 10,
+            "test__get_mask__is_consistent_regardless_of_inputs": 10,
+            "test__get_valid_actions__is_consistent_regardless_of_inputs": 10,
+            "test__sample_actions__get_logprobs__return_valid_actions_and_logprobs": 10,
+            "test__get_parents_step_get_mask__are_compatible": 10,
+            "test__sample_backwards_reaches_source": 10,
+            "test__state2readable__is_reversible": 20,
+            "test__gflownet_minimal_runs": 1,
+        }
+        self.n_states = {
+            "test__backward_actions_have_nonzero_forward_prob": 3,
+            "test__sample_backwards_reaches_source": 3,
+            "test__get_logprobs__all_finite_in_random_forward_transitions": 10,
+            "test__get_logprobs__all_finite_in_random_backward_transitions": 10,
+        }
+        self.batch_size = {
+            "test__sample_actions__get_logprobs__batched_forward_trajectories": 10,
+            "test__sample_actions__get_logprobs__batched_backward_trajectories": 10,
+            "test__get_logprobs__all_finite_in_accumulated_forward_trajectories": 10,
+            "test__gflownet_minimal_runs": 5,
+        }
