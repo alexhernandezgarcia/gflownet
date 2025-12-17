ed489cb4e15bf37bb3f85bf5ecf115a5ae02e66c
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2025-12-17 18:11:15 -0500

    Remove zero_logprobs

--- gflownet/gflownet.py
+++ gflownet/gflownet.py
@@ -975,7 +975,6 @@ class GFlowNetAgent:
                     self.opt.step()
                     self.lr_scheduler.step()
                     self.opt.zero_grad()
-                    batch.zero_logprobs()
 
             # Log training iteration: progress bar, buffer, metrics, intermediate
             # models
