e9abebab13d8d50e41b0b1f63625e61562e656fb
Alex <alexhg@e.email>
2026-03-27 16:59:49 -0400

    Node: add cube_kwargs parameter

--- gflownet/envs/tree/node.py
+++ gflownet/envs/tree/node.py
@@ -2,7 +2,7 @@
 Node of a decision tree.
 """
 
-from typing import Sequence
+from typing import Dict, Optional, Sequence
 
 from gflownet.envs.choice import Choice
 from gflownet.envs.composite.stack import Stack
@@ -14,6 +14,7 @@ class DecisionTreeNode(Stack):
     def __init__(
         self,
         features: Sequence,
+        cube_kwargs: Optional[Dict] = None,
         **kwargs,
     ):
         """
@@ -23,8 +24,10 @@ class DecisionTreeNode(Stack):
             The set of features.
         """
         self.features = features
+        self.cube_kwargs = cube_kwargs or {}
+
         self.stage_feature = 0
         self.feature = Choice(self.features, **kwargs)
         self.stage_threshold = 1
-        self.threshold = ContinuousCube(n_dim=1, **kwargs)
+        self.threshold = ContinuousCube(n_dim=1, **self.cube_kwargs)
         super().__init__(subenvs=tuple([self.feature, self.threshold]), **kwargs)
