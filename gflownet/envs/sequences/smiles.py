7c064613e1b68411a260054039a504a499c8982b
Dionessa <dionessa.biton@gmail.com>
2026-05-21 15:28:14 -0400

    included only tokens for organic molecules and other symbols

--- gflownet/envs/sequences/smiles.py
+++ gflownet/envs/sequences/smiles.py
@@ -6,7 +6,7 @@ import numpy as np
 from torchtyping import TensorType
 
 from gflownet.envs.sequences.base import SequenceBase
-from gflownet.envs.sequences.smiles_vocab import vocab as SMILES_VOCAB
+from gflownet.envs.sequences.smiles_organic_vocab import vocab as SMILES_VOCAB
 from gflownet.utils.common import tlong
 
 
