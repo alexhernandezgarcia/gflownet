36cf579a1b94cd4fa1ec3b2135d91176f28cf5de
AlexandraVolokhova <s-volohova@yandex.ru>
2026-07-19 08:39:25 +0900

    more tests for ctorus

--- tests/gflownet/envs/test_ctorus.py
+++ tests/gflownet/envs/test_ctorus.py
@@ -1,15 +1,31 @@
+from copy import copy
+from itertools import chain
+
 import common
 import numpy as np
 import pytest
 import torch
+from scipy import special
+from torch.distributions import Uniform
 
 from gflownet.envs.ctorus import ContinuousTorus
+from gflownet.proxy.torus import Torus
 from gflownet.utils.common import tbool, tfloat
 
 
 @pytest.fixture
 def env():
     return ContinuousTorus(n_dim=2, length_traj=3)
+
+
+@pytest.fixture
+def proxy():
+    return Torus(normalize=True, alpha=1.0, beta=1.0, device="cpu", float_precision=32)
+
+
+@pytest.fixture
+def env_su():
+    return ContinuousTorus(n_dim=2, length_traj=3, start_uniform=True)
 
 
 @pytest.mark.parametrize(
@@ -103,6 +119,86 @@ def test__sample_actions_batch__not_special_cases(
     assert action_sampled != action_special
 
 
+# Copypaste tests for start_uniform=True
+@pytest.mark.parametrize(
+    "state, done, is_backward, action_special",
+    [
+        ([0.0, 0.0, 2.0], False, False, (np.inf, np.inf)),
+        ([0.0, 0.0, 3.0], False, True, (np.inf, np.inf)),
+        ([1.37, 2.49, 2.0], False, False, (np.inf, np.inf)),
+        ([1.37, 2.49, 2.0], False, True, (np.inf, np.inf)),
+        ([0.0, 0.0, 2.0], False, True, (0.0, 0.0)),
+        ([1.37, 2.49, 2.0], False, True, (1.37, 2.49)),
+        ([1.37, 2.49, 1.0], False, False, (1.37, 2.49)),
+    ],
+)
+def test__sample_actions_batch__not_special_cases_su(
+    env_su, state, done, is_backward, action_special
+):
+    """
+    Test a few seemingly special cases, both forward and backward, and check that the
+    special action is not sampled. Some of the tests may fail once in a blue moon if at
+    all.
+    """
+    env = env_su
+    env.set_state(state, done=done)
+    if is_backward:
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_backward(), device=env.device), 0
+        )
+    else:
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_forward(), device=env.device), 0
+        )
+    random_policy = torch.unsqueeze(env.random_policy_output, 0)
+    action_sampled = env.sample_actions_batch(
+        random_policy,
+        mask,
+        [state],
+        is_backward,
+    )[0]
+    assert action_sampled != action_special
+
+
+@pytest.mark.parametrize(
+    "state, done, is_backward, action_expected",
+    [
+        ([0.0, 0.0, 3.0], False, False, (np.inf, np.inf)),
+        ([0.0, 0.0, 3.0], True, False, (np.inf, np.inf)),
+        ([0.0, 0.0, 3.0], True, True, (np.inf, np.inf)),
+        ([1.37, 2.49, 3.0], False, False, (np.inf, np.inf)),
+        ([1.37, 2.49, 3.0], True, False, (np.inf, np.inf)),
+        ([1.37, 2.49, 3.0], True, True, (np.inf, np.inf)),
+        ([0.0, 0.0, 1.0], False, True, (0.0, 0.0)),
+        ([1.37, 2.49, 1.0], False, True, (1.37, 2.49)),
+    ],
+)
+def test__sample_actions_batch__special_cases_su(
+    env_su, state, done, is_backward, action_expected
+):
+    """
+    Test a few of all (known...) special cases, both forward and backward.
+    """
+    env = env_su
+    env.set_state(state, done=done)
+    if is_backward:
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_backward(), device=env.device), 0
+        )
+    else:
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_forward(), device=env.device), 0
+        )
+    random_policy = torch.unsqueeze(env.random_policy_output, 0)
+    action_sampled = env.sample_actions_batch(
+        random_policy,
+        mask,
+        [state],
+        is_backward,
+    )[0]
+    assert all(np.isclose(action_sampled, action_expected))
+
+
 class TestContinuousTorusBasic(common.BaseTestsContinuous):
     @pytest.fixture(autouse=True)
     def setup(self, env):
@@ -132,3 +228,171 @@ class TestContinuousTorusBasic(common.BaseTestsContinuous):
             "test__get_logprobs__all_finite_in_accumulated_forward_trajectories": 10,
             "test__gflownet_minimal_runs": 10,
         }
+        self.n_states = {}  # TODO: Populate.
+
+
+def test__sample_start_uniform(env_su):
+    # test that first step is sampled from uniform and others are from policy
+    # test that first logprob is uniform
+    states = [
+        [0.0, 0.0, 1.0],
+        [0.0, 0.0, 2.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+    ]
+    masks = []
+    for state in states:
+        env_su.set_state(state, done=False)
+        mask = env_su.get_mask_invalid_actions_forward()
+        masks.append(mask)
+    masks = torch.tensor(masks, device=env_su.device)
+    distr_params = {
+        "vonmises_mean": 0.0,
+        "vonmises_concentration": 8.0,
+    }
+    policy_output = torch.stack([env_su.get_policy_output(distr_params)] * len(states))
+    unilogprob = -env_su.n_dim * np.log(2 * np.pi)
+    list_actions_first = []
+    list_actions_others = []
+    for _ in range(300):
+        actions = env_su.sample_actions_batch(
+            policy_output,
+            masks,
+            states,
+            is_backward=False,
+        )
+        logprobs = env_su.get_logprobs(
+            policy_output, actions, masks, states, is_backward=False
+        )
+        assert all(np.isclose(logprobs[2:].detach().cpu().numpy(), unilogprob))
+        assert not all(np.isclose(logprobs[:2].detach().cpu().numpy(), unilogprob))
+        list_actions_first.extend(list(chain(*actions[2:])))
+        list_actions_others.extend(list(chain(*actions[:2])))
+
+    assert np.isclose(np.mean(list_actions_first), np.pi, atol=0.1)
+    assert np.isclose(np.std(list_actions_first) ** 2, np.pi**2 / 3, atol=0.5)
+    assert np.isclose(np.mean(list_actions_others), 0.0, atol=0.1)
+    conc = distr_params["vonmises_concentration"]
+    vm_var = 1 - special.i1(conc) / special.i0(conc)
+    assert np.isclose(np.std(list_actions_others) ** 2, vm_var, atol=0.1)
+
+
+def test__get_logprobs_start_uniform(env_su):
+    states = [
+        [0.0, 0.0, 1.0],
+        [0.0, 0.0, 2.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+    ]
+    n_from_source = 3
+    from_source = torch.tensor([False, False, True, True, True])
+    masks = []
+    for state in states:
+        env_su.set_state(state, done=False)
+        mask = env_su.get_mask_invalid_actions_forward()
+        masks.append(mask)
+    masks = torch.tensor(masks, device=env_su.device)
+    distr_params = {
+        "vonmises_mean": 0.0,
+        "vonmises_concentration": 8.0,
+    }
+    policy_output = torch.stack([env_su.get_policy_output(distr_params)] * len(states))
+    actions = env_su.sample_actions_batch(
+        policy_output,
+        masks,
+        states,
+        is_backward=False,
+    )
+    actions = torch.tensor(actions)
+    logprobs = env_su.get_logprobs(
+        policy_output, actions, masks, states, is_backward=False
+    )
+    distr_fs_angles = Uniform(
+        torch.zeros(n_from_source, env_su.n_dim).to(actions),
+        2 * torch.pi * torch.ones(n_from_source, env_su.n_dim).to(actions),
+    )
+    expected_logprobs = distr_fs_angles.log_prob(actions[from_source]).sum(axis=1)
+    assert torch.allclose(logprobs[from_source], expected_logprobs)
+    assert logprobs[0] != logprobs[-1]
+    assert logprobs[1] != logprobs[-1]
+
+
+def test__backward_logprob(env):
+    for _ in range(100):
+        angles = np.random.rand(2).tolist()
+        state = angles + [1.0]
+        env.set_state(state, done=False)
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_backward(), device=env.device), 0
+        )
+        distr_params = {
+            "vonmises_mean": 0.0,
+            "vonmises_concentration": 8.0,
+        }
+        policy_output = env.get_policy_output(distr_params).unsqueeze(0)
+        actions = torch.tensor([angles])
+        logprobs = env.get_logprobs(
+            policy_output, actions, mask, [state], is_backward=True
+        )
+        assert torch.allclose(logprobs, torch.tensor([0.0]))
+        actions_rand = torch.tensor(np.random.rand(1, 2).tolist())
+        logprobs_rand = env.get_logprobs(
+            policy_output, actions_rand, mask, [state], is_backward=True
+        )
+        assert ~torch.isfinite(logprobs_rand)
+
+
+def test__backward_logprob_su(env_su):
+    env = env_su
+    for _ in range(100):
+        angles = np.random.rand(2).tolist()
+        state = angles + [1.0]
+        env.set_state(state, done=False)
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_backward(), device=env.device), 0
+        )
+        distr_params = {
+            "vonmises_mean": 0.0,
+            "vonmises_concentration": 8.0,
+        }
+        policy_output = env.get_policy_output(distr_params).unsqueeze(0)
+        actions = torch.tensor([angles])
+        logprobs = env.get_logprobs(
+            policy_output, actions, mask, [state], is_backward=True
+        )
+        assert torch.allclose(logprobs, torch.tensor([0.0]))
+        actions_rand = torch.tensor(np.random.rand(1, 2).tolist())
+        logprobs_rand = env.get_logprobs(
+            policy_output, actions_rand, mask, [state], is_backward=True
+        )
+        assert ~torch.isfinite(logprobs_rand)
+
+
+def tests__unroll_trajectory(env, proxy):
+    traj_states = []
+    traj_actions = []
+
+    proxy.setup(env)
+
+    env.reset()
+
+    done = False
+
+    while not done:
+        traj_states.append(copy(env.state))
+        random_policy = torch.unsqueeze(env.random_policy_output, 0)
+        mask = torch.unsqueeze(
+            tbool(env.get_mask_invalid_actions_forward(), device=env.device), 0
+        )
+        actions = env.sample_actions_batch(
+            random_policy,
+            mask,
+            [env.state],
+            is_backward=False,
+        )
+        traj_actions.append(actions[0])
+        env.step(actions[0])
+        done = env.done
+    assert len(traj_states) == len(traj_actions) == env.length_traj + 1
